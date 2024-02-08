e41b92885ed8c7224101641e9064e453c5758777
SangJung <sangjung245@gmail.com>
2024-02-08 17:15:44 +0900

    [FEAT] 프로젝트 상수 추가

---
+++ project_constants.py
@@ -0,0 +1,6 @@
+PROJECT_NAME = 'Facial Classifier'
+PROJECT_VERSION = '0.1'
+PROJECT_DESCRIPTION = 'Facial classifier using machine learning'
+PROJECT_URL = 'https://github.com/sangjung0/Facial_Classifier'
+PROJECT_AUTHOR = 'sangjung'
+PROJECT_AUTHOR_EMAIL = 'sangjung245@gmail.com'
