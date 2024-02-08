54fcb22346865f6f087f256238d8d9c4d7cc0f31
SangJung <sangjung245@gmail.com>
2024-02-08 17:17:17 +0900

    [FEAT] 패키지 추가

---
+++ test/__init__.py
@@ -0,0 +1,8 @@
+"""
+unit 테스트 형시이 아닌, 개발과정에서 사용 될 테스트 코드들의 패키지
+"""
+
+from test.video_test import readTest
+
+__all__ = ['readTest']
+__version__ = '0.1'
---
+++ util/__init__.py
@@ -0,0 +1,8 @@
+"""
+유틸 패키지
+"""
+
+from util.VideoData import VideoData
+
+__all__ = [VideoData]
+__version__ = '0.1'
