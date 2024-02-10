c99ec6e9eed1367f25d3e05d64b3208b479ecea2
SangJung <sangjung245@gmail.com>
2024-02-10 20:52:44 +0900

    [TEST] 테스트 코드 추가

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from test import readTest
+from test import readTest,mtcnnTest,haarTest
 from test_constants import TEST_VIDEO
 
-readTest(TEST_VIDEO)
+#readTest(TEST_VIDEO)
+#mtcnnTest(TEST_VIDEO)
+haarTest(TEST_VIDEO)
--- test/video_test.py
+++ test/video_test.py
@@ -1,7 +1,16 @@
 from util import VideoData
+from face_detector import MyMTCNN, HaarCascade
 
 def readTest(fileName):
     video = VideoData(fileName)
     video.play()
 
+def mtcnnTest(fileName):
+    video = VideoData(fileName)
+    mtcnn = MyMTCNN()
+    video.play(mtcnn.detect)
 
+def haarTest(fileName):
+    video = VideoData(fileName)
+    haar = HaarCascade()
+    video.play(haar.detect)
