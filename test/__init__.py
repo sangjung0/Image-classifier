ad522e587c9e5f33963f97884344372a1770e869
SangJung <sangjung245@gmail.com>
2024-02-10 20:55:11 +0900

    [TEST] test 패키지 init에 모듈 추가

--- test/__init__.py
+++ test/__init__.py
@@ -2,7 +2,7 @@
 unit 테스트 형시이 아닌, 개발과정에서 사용 될 테스트 코드들의 패키지
 """
 
-from test.video_test import readTest
+from test.video_test import readTest, mtcnnTest, haarTest
 
-__all__ = ['readTest']
+__all__ = ['readTest', 'mtcnnTest', 'haarTest']
 __version__ = '0.1'
