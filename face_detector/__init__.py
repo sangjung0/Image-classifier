942a9202aaaf10524d25cda0aa8a0c9bcc2fa634
SangJung <sangjung245@gmail.com>
2024-02-12 00:02:26 +0900

    [FEAT] 얼굴 탐지 필터 모듈 추가

--- face_detector/__init__.py
+++ face_detector/__init__.py
@@ -4,6 +4,7 @@
 
 from face_detector.MyMTCNN import MyMTCNN
 from face_detector.HaarCascade import HaarCascade
+from face_detector.FaceDetectorFilter import FaceDetectorFilter
 
-__all__ = ['MyMTCNN', 'HaarCascade']
+__all__ = ['MyMTCNN', 'HaarCascade', 'FaceDetectorFilter']
 __version__ = '0.1'
