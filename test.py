e5636383f604dcd48cfc9a3dbc9879267173bc9a
SangJung <sangjung245@gmail.com>
2024-02-11 16:54:48 +0900

    [TEST] 테스트 코드 추가

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-from test import readTest,mtcnnTest,haarTest
+from test import video_test
 from test_constants import TEST_VIDEO
 
-#readTest(TEST_VIDEO)
-#mtcnnTest(TEST_VIDEO)
-haarTest(TEST_VIDEO)
+#video_test.readTest(TEST_VIDEO)
+#video_test.mtcnnTest(TEST_VIDEO)
+#video_test.haarTest(TEST_VIDEO)
+#video_test.imgFilterTest(TEST_VIDEO)
+#video_test.trackingTest(TEST_VIDEO)
+#video_test.trackingTest2(TEST_VIDEO)
+video_test.sceneChangeTest(TEST_VIDEO)
+
--- test/video_test.py
+++ test/video_test.py
@@ -1,5 +1,8 @@
 from util import VideoData
+from util import imgFilters
 from face_detector import MyMTCNN, HaarCascade
+from face_tracker import LucasKanade, GunnerFarneback
+from util import CalcHistogram
 
 def readTest(fileName):
     video = VideoData(fileName)
@@ -14,3 +17,24 @@ def haarTest(fileName):
     video = VideoData(fileName)
     haar = HaarCascade()
     video.play(haar.detect)
+
+def imgFilterTest(fileName):
+    video = VideoData(fileName)
+    video.play( filter = lambda x : imgFilters.bilateralFiltering(x, inplace=True))
+
+def trackingTest(fileName):
+    video = VideoData(fileName)
+    tracker = LucasKanade()
+    video.play( tracking= lambda x, y : tracker.tracking(x, inplace=True, isNewScene=y))
+
+def trackingTest2(fileName):
+    video = VideoData(fileName)
+    tracker = GunnerFarneback()
+    video.play( tracking = lambda x, y : tracker.tracking(x, inplace=True))
+
+    
+def sceneChangeTest(fileName):
+    video = VideoData(fileName)
+    tracker = LucasKanade()
+    change = CalcHistogram()
+    video.play( tracking= lambda x, y : tracker.tracking(x, inplace=True, isNewScene=y), isNewScene=change.isNewScene)
