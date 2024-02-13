f7923754cb6085cff00ec9d3e665893c26436817
sangjung <sangjung245@gmail.com>
2024-02-13 21:38:04 +0900

    [MODIFY] 파일 이동에 따른 패키지 제거

--- util/__init__.py
+++ util/__init__.py
@@ -2,9 +2,9 @@
 유틸 패키지
 """
 
-from util.VideoData import VideoData
+
 from util import imgFilters
 from util.sceneChanges import CalcHistogram, CalcEdge
 
-__all__ = ['VideoData', 'imgFilters', 'CalcHistogram', 'CalcEdge']
+__all__ = ['imgFilters', 'CalcHistogram', 'CalcEdge']
 __version__ = '0.1'
