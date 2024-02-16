2208c9c55d24faddd7e2fc485923ec27926e5f1c
sangjung <sangjung245@gmail.com>
2024-02-16 20:15:21 +0900

    [FEAT] 비디오 프로세서 생성기 추가

--- video/VideoProcessor.py
+++ video/VideoProcessor.py
@@ -1,27 +1,22 @@
 from video.VideoSection import VideoSection
+from multiprocessing import Queue, Value
+from project_constants import STOP, PAUSE, END_OF_LOAD
+import pickle
+import time 
 
 class VideoProcessor:
-    def __init__(self, videoSection:VideoSection, detector = None, tracker = None, draw:bool = False, sceneDetector = None ):
-        self.videoSection = videoSection
+    def __init__(self, detector = None, tracker = None, sceneDetector = None, draw:bool = False ):
         self.__detector = detector
         self.__tracker = tracker
         self.__draw = draw
         self.__sceneDetector = sceneDetector
 
-    @property
-    def videoSection(self):
-        return self.__videoSection
-    @videoSection.setter
-    def videoSection(self, value):
-        if isinstance(value,VideoSection):
-            self.__videoSection = value
-        else: raise ValueError("Video section must be VideoSection")
-
-    def processing(self):
+    def processing(self, videoSection:VideoSection):
+        print(videoSection.index, "처리 시작")
         tracker = None
         if  self.__tracker is not None:
             tracker = self.__tracker.getTracker() #고민해보자
-        for frame in self.__videoSection.frameAry:
+        for frame in videoSection.frameAry:
             isNewScene = False if self.__sceneDetector is None else self.__sceneDetector.isNewScene(frame.getFrame())
             if frame.isDetect or isNewScene:
                 if self.__detector is not None:
@@ -35,4 +30,28 @@ class VideoProcessor:
             if tracker is not None:
                 trackingData = tracker.tracking(frame.getFrame(self.__tracker.colorConstant), isNewScene, self.__draw, frame.frame, frame.scale)
 
-        return self.__videoSection
+        print(videoSection.index, "처리 완료")
+        return videoSection
+    
+    def __call__(self, data: Queue, result: Queue, flag: Value): # type: ignore
+        while True:
+            if flag.value == PAUSE:
+                time.sleep(0.5)
+            elif flag.value == STOP:
+                break
+            else:
+                if not data.empty():
+                    result.put(pickle.dumps(self.processing(pickle.loads(data.get()))))
+                elif flag.value == END_OF_LOAD:
+                    break
+        print("연산 프로세서 종료")
+    
+class VideoProcessorGenerator:
+    def __init__(self, detector, tracker, sceneDetector, draw):
+        self.__detector = detector
+        self.__tracker = tracker
+        self.__sceneDetector = sceneDetector
+        self.__draw = draw
+
+    def generate(self):
+        return VideoProcessor(self.__detector, self.__tracker, self.__sceneDetector, self.__draw)
