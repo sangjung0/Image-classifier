b93b164a1b5c7a361848b8083cf3b6be840ed338
sangjung <sangjung245@gmail.com>
2024-02-16 20:14:39 +0900

    [MODIFY] 주석 제거

--- video/VideoData.py
+++ video/VideoData.py
@@ -2,20 +2,13 @@ import cv2
 import numpy as np
 
 class VideoData:    
-    def __init__(self, fileName = None):
-        self.fileName = fileName
+    def __init__(self, fileName):
+        self.__fileName = fileName
+        self.setVideoMetaData()
 
     @property
     def fileName(self):
         return self.__fileName
-    @fileName.setter
-    def fileName(self, value):
-        #Path 객체로 변경 할 것
-        if isinstance(value, str) and value.strip() != '':
-            self.__fileName = value
-            self.setVideoMetaData()
-        else: raise Exception("fileName 값 잘못 됨")
-
     @property
     def width(self):
         return self.__width
@@ -32,39 +25,6 @@ class VideoData:
         self.__height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.__fps = int(1000/cap.get(cv2.CAP_PROP_FPS))
         cap.release()
-
-    # def play(self, detector = None, filter = None, tracker = None, sceneDetector = None):
-    #     if(self.fileName is None): raise Exception("파일 이름 없음")
-
-    #     cap = VideoData.read(self.fileName)
-    #     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    #     delay = int(1000/cap.get(cv2.CAP_PROP_FPS))
-    #     count = 0
-
-    #     while(cap.isOpened()):
-    #         ret, frame = cap.read()
-    #         if count % self.detectFrame == 0:
-    #             count = 0
-
-    #         if ret:
-    #             resizedFrame = cv2.resize(frame, (width//self.__scale, height//self.__scale))
-    #             img = filter(resizedFrame) if filter is not None else resizedFrame
-    #             isNewScene = sceneDetector.isNewScene(img) if sceneDetector else False
-
-    #             if detector is not None and (count == 0 or isNewScene):
-    #                 faceLocation = detector.detect(img, draw=True, drawFrame=frame)
-
-    #             if tracker is not None:
-    #                 trackingData = tracker.tracking(img, isNewScene=isNewScene, draw=True, drawFrame=frame)
-
-    #             cv2.imshow(self.fileName, frame)
-    #         count += 1
-
-    #         if cv2.waitKey(delay) & 0xFF == ord('q'):
-    #             break
-    #     cap.release()
-    #     cv2.destroyAllWindows()
 
     def __enter__(self):
         self.__cap = VideoData.read(self.fileName)
@@ -89,7 +49,7 @@ class VideoData:
                 self.__prevFrame = frame
                 self.__index += 1
                 return self.__index, frame
-            self.__cap.release()
+            #self.__cap.release()
             return StopIteration
 
     @staticmethod
