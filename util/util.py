8e9248d016ac2b2e00631969d56301f81435a580
sangjung <sangjung245@gmail.com>
2024-02-18 16:43:16 +0900

    [FEAT] 로거 클래스, 타이머 클래스 추가

--- util/util.py
+++ util/util.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import timeit
+
 class AllProcessIsTerminated:
     def __init__(self, processes):
         self.__processes = processes
@@ -18,3 +21,43 @@ class AllTransmissionMediumIsTerminated:
         for m in self.__mediums:
             m.close()
             m.join_thread()
+
+class Timer:
+    def __init__(self):
+        self.__startTime = None
+        self.__endTime = None
+        self.__duration = None
+
+    @property
+    def startTime(self):
+        return self.__startTime
+
+    @property
+    def endTime(self):
+        return self.__endTime
+    
+    @property
+    def duration(self):
+        return self.__duration
+
+    def start(self):
+        self.__startTime = timeit.default_timer()
+        self.__endTime = None
+        self.__duration = None
+    
+    def end(self):
+        if self.__startTime is None: raise Exception("타이머 시작 안함")
+        self.__endTime = timeit.default_timer()
+        self.__duration = self.__endTime - self.__startTime
+
+    def __str__(self):
+        return f"Time: {round(self.duration, 5)}s"
+    
+class Loger:
+    def __init__(self, name, isPrint = True):
+        self.__name = name
+        self.__isPrint = isPrint
+
+    def __call__(self,*msg, timer:Timer = None):
+        if self.__isPrint:
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] - {self.__name.ljust(20)} - {str(timer if timer else None).ljust(20)}: ", " ".join(str(i) for i in msg))
