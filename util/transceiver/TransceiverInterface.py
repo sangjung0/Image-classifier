962f9f51581ec836a68c6ff3a70b9795a8f230fd
sangjung <sangjung245@gmail.com>
2024-02-18 13:46:23 +0900

    [ADD] 송수신 인터페이스 추가

---
+++ util/transceiver/TransceiverInterface.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from multiprocessing import Queue
+from typing import Union
+
+from util import CompressorInterface
+from util import SerializerInterface
+
+class TransceiverInterface(ABC):
+
+    def __init__(self, serializer:SerializerInterface, compressor: CompressorInterface):
+        self._compressor = compressor
+        self._serializer = serializer
+
+    @abstractmethod
+    def send(self, destination: Queue, value: object) -> None: pass
+
+    @abstractmethod
+    def receive(self, destination: Queue) -> Union[bool, object]: pass
