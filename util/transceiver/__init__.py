72c36d13776895d3bd9912ebd2e2f61abc77ccd6
sangjung <sangjung245@gmail.com>
2024-02-18 13:46:45 +0900

    [ADD] 송수신 패키지 init 추가

---
+++ util/transceiver/__init__.py
@@ -0,0 +1,10 @@
+"""
+송수신기 패키지
+"""
+
+
+from util.transceiver.TransceiverInterface import TransceiverInterface
+from util.transceiver.Transceiver import Transceiver
+
+__all__ = ['TransceiverInterface', 'Transceiver']
+__version__ = '0.1'
