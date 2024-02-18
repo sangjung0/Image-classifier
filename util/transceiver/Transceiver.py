97e0eff34219b40970735252e3e8c8a6f3ebc47f
sangjung <sangjung245@gmail.com>
2024-02-18 13:46:14 +0900

    [ADD] 송수신 클래스 추가

---
+++ util/transceiver/Transceiver.py
@@ -0,0 +1,12 @@
+
+from util.transceiver import TransceiverInterface
+
+class Transceiver(TransceiverInterface):
+
+    def send(self, destination, value):
+        destination.put(self._compressor.compress(self._serializer.serialization(value)))
+
+    def receive(self, destination):
+        if destination.empty():
+            return False, None
+        return True, self._serializer.deSerialization(self._compressor.decompress(destination.get()))
