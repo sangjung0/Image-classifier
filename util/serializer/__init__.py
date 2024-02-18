55ddebdb682062e6abb268f418c3340444f14718
sangjung <sangjung245@gmail.com>
2024-02-18 13:46:02 +0900

    [ADD] 직렬화 패키지 init 추가

---
+++ util/serializer/__init__.py
@@ -0,0 +1,10 @@
+"""
+직렬화 패키지
+"""
+
+
+from util.serializer.PickleSerializer import PickleSerializer
+from util.serializer.SerializerInterface import SerializerInterface
+
+__all__ = ['PickleSerializer', 'SerializerInterface']
+__version__ = '0.1'
