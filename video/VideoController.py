5f10dc7522200f3c76ebd4e681dca9c0d709241a
sangjung <sangjung245@gmail.com>
2024-02-18 14:25:22 +0900

    [FEAT] 프로세서 확인 종료 객체 추가

--- video/VideoController.py
+++ video/VideoController.py
@@ -8,7 +8,7 @@ from video.VideoLoader import VideoLoader
 from video.VideoProcessor import VideoProcessor
 from video.VideoDistributor import VideoDistributor
 from project_constants import PAUSE
-from util.util import AllProcessIsTerminated
+from util.util import AllProcessIsTerminated, AllTransmissionMediumIsTerminated
 from util.compressor import CompressorInterface, UnCompressor
 from util.serializer import PickleSerializer
 from util.transceiver import TransceiverInterface, Transceiver
@@ -17,7 +17,7 @@ class VideoController:
 
     @staticmethod
     def startAndGetVideoLoader(
-        fileName:str, processorNumber:int = 4, detectFrameCount:int = 1, scale:int = 1, cfl:int = 50, 
+        fileName:str, processorNumber:int = 3, detectFrameCount:int = 1, scale:int = 1, cfl:int = 50, 
         transceiver:TransceiverInterface = Transceiver(PickleSerializer(), UnCompressor()), 
         filter = None, detector = None, tracker = None, sceneDetector = None, draw:bool = False
         ):
@@ -30,13 +30,17 @@ class VideoController:
         flag = Value('i',PAUSE)
         lastIndex = Value('i', 0)
 
-        Process(target=VideoDistributor(videoData, scale, detectFrameCount, cfl, filter), args=(data, flag, lastIndex, transceiver)).start()
+        videoDistributorProcess = Process(target=VideoDistributor(videoData, scale, detectFrameCount, cfl, filter), args=(data, flag, lastIndex, transceiver)).start()
 
         for _ in range(processorNumber):
             p = Process(target=VideoProcessor(detector, tracker, sceneDetector, draw), args=(data, result, flag, transceiver))
             p.start()
             processes.append(p)
 
-        Thread(target=videoBuffer, args=(result, flag, AllProcessIsTerminated(processes).allProcessIsTerminated, transceiver)).start()
+        videoBufferThread = Thread(target=videoBuffer, args=(result, flag, AllProcessIsTerminated(processes).allProcessIsTerminated, transceiver)).start()
 
-        return VideoLoader(videoData, videoBuffer, flag, lastIndex)
+        return VideoLoader(
+            videoData, videoBuffer, flag, lastIndex, 
+            AllProcessIsTerminated(processes+[videoDistributorProcess, videoBufferThread]).wait,
+            AllTransmissionMediumIsTerminated([data,result]).wait
+            )
