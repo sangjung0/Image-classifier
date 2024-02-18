5c99b46eac61cbd4a06ec7e02fed6fefee8f1d20
sangjung <sangjung245@gmail.com>
2024-02-18 14:23:55 +0900

    [MODIFY] 압축 메소드 수정

--- util/compressor/GZipCompressor.py
+++ util/compressor/GZipCompressor.py
@@ -6,9 +6,10 @@ from util.compressor.CompressorInterface import CompressorInterface
 class GZipCompressor(CompressorInterface):
 
     def compress(self, value):
-        with gzip.GzipFile(fileobj=io.BytesIO(), mode='wb') as f:
-            f.write(value)
-            return f.getvalue()
+        with io.BytesIO() as buf:
+            with gzip.GzipFile(fileobj=buf, mode='wb') as f:
+                f.write(value)
+            return buf.getvalue()
         
     def decompress(self, value):
         with gzip.GzipFile(fileobj=io.BytesIO(value), mode='rb') as f:
