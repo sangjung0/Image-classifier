144522a3e5a952c84856c802195cdb7b2fd80feb
sangjung <sangjung245@gmail.com>
2024-02-18 13:47:32 +0900

    [FEAT] 비디오 버퍼에 송수신기 추가

--- video/VideoBuffer.py
+++ video/VideoBuffer.py
@@ -1,9 +1,10 @@
-from video.model import VideoSection
 from multiprocessing import Queue, Value
-from project_constants import STOP, PAUSE, RUN
 from threading import Lock
-import pickle
 import bisect
+
+from util.transceiver import TransceiverInterface
+from video.model import VideoSection
+from project_constants import STOP, PAUSE, RUN
 class VideoBuffer:
     def __init__(self):
         self.__videoSections = []
@@ -19,14 +20,15 @@ class VideoBuffer:
                 return self.__videoSections.pop(0)
             return None
     
-    def __call__(self, result:Queue, flag:Value, finish): # type: ignore
+    def __call__(self, result:Queue, flag:Value, finish, transceiver:TransceiverInterface): # type: ignore
         while True:
             if flag.value == STOP:
                 break
             else:
-                if not result.empty():
+                ret, data = transceiver.receive(result)
+                if ret:
+                    self.append(data)
                     print("버퍼에 담김")
-                    self.append(pickle.loads(result.get()))
                 elif finish():
                     flag.value = STOP
         print("버퍼 쓰레드 종료")
