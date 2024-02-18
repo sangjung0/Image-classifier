6584961b0c7217ac7a18555dbbe0fc3887c68aa4
sangjung <sangjung245@gmail.com>
2024-02-18 18:59:41 +0900

    [FEAT] JPG 압축 클래스 추가

--- util/__init__.py
+++ util/__init__.py
@@ -5,14 +5,14 @@
 
 from util.sceneChanges import CalcHistogram, CalcEdge
 from util.serializer import PickleSerializer, SerializerInterface
-from util.compressor import CompressorInterface, UnCompressor, GZipCompressor
+from util.compressor import CompressorInterface, UnCompressor, GZipCompressor, JpgCompressor
 from util.transceiver import TransceiverInterface, Transceiver
 from util import util
 
 __all__ = [
     'CalcHistogram', 'CalcEdge','util', 
     'PickleSerializer', 'SerializerInterface',
-    'UnCompressor', 'CompressorInterface','GZipCompressor',
+    'UnCompressor', 'CompressorInterface','GZipCompressor', 'JpgCompressor',
     'TransceiverInterface', 'Transceiver'
     ]
 __version__ = '0.1'
--- util/compressor/__init__.py
+++ util/compressor/__init__.py
@@ -6,6 +6,7 @@
 from util.compressor.CompressorInterface import CompressorInterface
 from util.compressor.GZipCompressor import GZipCompressor
 from util.compressor.UnCompressor import UnCompressor
+from util.compressor.JpgCompressor import JpgCompressor
 
-__all__ = ['CompressorInterface', 'GZipCompressor', 'UnCompressor']
+__all__ = ['CompressorInterface', 'GZipCompressor', 'UnCompressor', 'JpgCompressor']
 __version__ = '0.1'
