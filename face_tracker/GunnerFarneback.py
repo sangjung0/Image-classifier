f786a36c3ae6f74b327b9e35d939199b4be8d547
sangjung <sangjung245@gmail.com>
2024-02-19 22:30:35 +0900

    [FEAT] 트랙커 인터페이스 구현

--- face_tracker/GunnerFarneback.py
+++ face_tracker/GunnerFarneback.py
@@ -1,28 +1,43 @@
 import cv2
 import numpy as np
 
+from face_tracker.TrackerInterface import TrackerInterface
+
 #https://bkshin.tistory.com/entry/OpenCV-31-%EA%B4%91%ED%95%99-%ED%9D%90%EB%A6%84Optical-Flow
-class GunnerFarneback:
-    STEP = 64
-    def __init__(self):
+#작동유무 확인 안함
+class GunnerFarneback(TrackerInterface):
+    def __init__(self, step:int = 64, color = cv2.COLOR_BGR2GRAY):
+        self.__prevImg = None
+        self.__step = step
+        self.__color = color
+
+    @property
+    def colorConstant(self):
+        return self.__color
+    
+    def clear(self):
         self.__prevImg = None
 
-    def tracking(self, img, color = cv2.COLOR_BGR2GRAY, inplace=False):
-        cntImg = img if inplace else img.copy()
-        gray = cv2.cvtColor(cntImg, color)
+    def tracking(self, img, scale:int = 1, isNewScene:bool = False, draw:bool = False, drawFrame:np.ndarray = None):
+        step = self.__step
+
+        if isNewScene: self.clear()
         if self.__prevImg is None:
-            self.__prevImg = gray
-        else:
-            flow = cv2.calcOpticalFlowFarneback(self.__prevImg, gray, None, 0.5, 3, 15, 3, 5, 1.1,cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+            self.__prevImg = img
+            return []
+        
+        flow = cv2.calcOpticalFlowFarneback(self.__prevImg, img, None, 0.5, 3, 15, 3, 5, 1.1,cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+        self.__prevImg = img
 
-            h, w = cntImg.shape[:2]
+        h, w = img.shape[:2]
 
-            idx_y, idx_x = np.mgrid[GunnerFarneback.STEP/2:h:GunnerFarneback.STEP, GunnerFarneback.STEP/2:w:GunnerFarneback.STEP].astype(np.int32)
-            indices = np.stack((idx_x, idx_y), axis = -1).reshape(-1,2)
+        idx_y, idx_x = np.mgrid[step/2:h:step, step/2:w:step].astype(np.int32)
+        indices = np.stack((idx_x, idx_y), axis = -1).reshape(-1,2)
 
+        if draw:
             for x,y in indices:
-                cv2.circle(img, (x,y), 1, (0,255,0), -1)
-                dx,dy = flow[y,x].astype(np.int32)
-                cv2.line(img, (x,y), (x+dx, y+dy), (0,255,0),2,cv2.LINE_AA)
+                cv2.circle(drawFrame, (x*scale,y*scale), 1, (0,255,0), -1)
+                dx,dy = flow[y*scale,x*scale].astype(np.int32)
+                cv2.line(img, (x*scale,y*scale), ((x+dx)*scale, (y+dy)*scale), (0,255,0),2,cv2.LINE_AA)
 
-            self.__prevImg = gray
+        return [] # 수정 필요
