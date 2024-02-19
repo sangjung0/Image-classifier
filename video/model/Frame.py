a4c00d7cf0a68df08fa9ee0567d449260fa7550d
sangjung <sangjung245@gmail.com>
2024-02-19 22:35:33 +0900

    [UPDATE] 데이터 상수화

--- video/model/Frame.py
+++ video/model/Frame.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from project_constants import FRAME_SCALE, FRAME_FRAME
+
 class Frame:
     def __init__(self, index:int, frame:np.ndarray, width:int, height:int, rWidth:int, rHeight:int, scale:int, isDetect:bool, filter: object = None):
         self.__index = index
@@ -11,7 +13,7 @@ class Frame:
         self.__scale = scale
         self.__isDetect = isDetect
         self.__filter = filter
-        self.__imgs = {'frame': frame}
+        self.__imgs = {FRAME_FRAME: frame}
 
     @property
     def imgs(self):
@@ -19,36 +21,30 @@ class Frame:
 
     @property
     def frame(self):
-        return self.__imgs['frame']
+        return self.__imgs[FRAME_FRAME]
     @property
     def index(self):
         return self.__index
     @property
     def isDetect(self):
         return self.__isDetect
+    @isDetect.setter
+    def isDetect(self, value):
+        if isinstance(value, bool):
+            self.__isDetect = value
+        else:
+            raise ValueError("isDetect is must be bool")
     @property
     def scale(self):
         return self.__scale
 
     # cv2 COLOR 상수 와야함
-    def getFrame(self, cv2Constant = 'scale') -> np.ndarray:
+    def getFrame(self, cv2Constant = FRAME_SCALE) -> np.ndarray:
         if cv2Constant in self.__imgs:
             return self.__imgs[cv2Constant]
-        if cv2Constant == 'scale':
+        if cv2Constant == FRAME_SCALE:
             self.__imgs[cv2Constant] = self.__filter(cv2.resize(self.frame, (self.__rWidth, self.__rHeight))) if self.__filter is not None else cv2.resize(self.frame, (self.__rWidth, self.__rHeight))
         else:
             self.__imgs[cv2Constant] = cv2.cvtColor(self.getFrame(), cv2Constant) 
         return self.__imgs[cv2Constant]
     
-    class FrameGenerator:
-        def __init__(self, width, height, scale, filter, isDetect):
-            self.__width = width
-            self.__height = height
-            self.__rWidth = width // scale
-            self.__rHeight =height // scale
-            self.__scale = scale
-            self.__isDetect = isDetect
-            self.__filter = filter
-
-        def generate(self, index, frame) :
-            return Frame(index, frame, self.__width, self.__height, self.__rWidth, self.__rHeight, self.__scale, self.__isDetect(index), self.__filter)
