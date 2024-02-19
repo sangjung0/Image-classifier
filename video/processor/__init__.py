12f272af99036a6f2dfccb7e79e30515e99ef122
sangjung <sangjung245@gmail.com>
2024-02-19 22:36:47 +0900

    [FEAT] 프로세서 패키지 추가

---
+++ video/processor/__init__.py
@@ -0,0 +1,9 @@
+"""
+연산 프로세서 
+"""
+
+from video.processor.DetectProcessor import DetectProcessor
+from video.processor.VisionProcessor import VisionProcessor
+
+__all__ = ['DetectProcessor', 'VisionProcessor']
+__version__='0.1'
