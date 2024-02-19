7b5747d98165dc7d0181617615a2b70a9abc95ec
sangjung <sangjung245@gmail.com>
2024-02-19 22:27:16 +0900

    [UPDATE] 상수 수정

--- project_constants.py
+++ project_constants.py
@@ -4,13 +4,18 @@ PROJECT_DESCRIPTION = 'Facial classifier using machine learning'
 PROJECT_URL = 'https://github.com/sangjung0/Facial_Classifier'
 PROJECT_AUTHOR = 'sangjung'
 PROJECT_AUTHOR_EMAIL = 'sangjung245@gmail.com'
-FACE = 'face'
-NOSE = 'nose'
-MOUTH = 'mouth'
-EYE = 'eye'
-FACE_MODEL = './face_detector/model/haarcascade_frontalface_default.xml'
-EYE_MODEL = './face_detector/model/haarcascade_eye.xml'
-RUN = 100
-STOP = 0
-PAUSE = 101
-END_OF_LOAD = 102
+
+DETECTOR_FACE = 'face'
+DETECTOR_NOSE = 'nose'
+DETECTOR_MOUTH = 'mouth'
+DETECTOR_EYE = 'eye'
+
+HAARCASCADE_FACE_MODEL = './face_detector/model/haarcascade_frontalface_default.xml'
+HAARCASCADE_EYE_MODEL = './face_detector/model/haarcascade_eye.xml'
+
+PROCESSOR_RUN = 100
+PROCESSOR_STOP = 0
+PROCESSOR_PAUSE = 101
+
+FRAME_SCALE = 'scale'
+FRAME_FRAME = 'frame'
