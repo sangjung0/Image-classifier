a3580e85c427f53ade43824871ff3858a15548e0
SangJung <sangjung245@gmail.com>
2024-02-26 02:04:31 +0000

    [FEAT] 쓰레드 추가

--- video/processor/ProcessorInterface.py
+++ video/processor/ProcessorInterface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from multiprocessing import Queue, Value
+from threading import Thread, Lock
 import os
 import time
 
@@ -10,8 +11,12 @@ from project_constants import PROCESSOR_STOP, PROCESSOR_PAUSE
 
 class ProcessorInterface(ABC):
 
-    def __init__(self, name:str):
+    def __init__(self, name:str, bufSize=3):
         self.__name = name
+        self.__data = []
+        self.__result = []
+        self.__bufSize= bufSize
+        self.__lock = Lock()
 
     @abstractmethod
     def __prepare__(self) -> None: pass
@@ -19,37 +24,69 @@ class ProcessorInterface(ABC):
     @abstractmethod
     def processing(self, section: Section): pass
 
-    def __call__(self, data: Queue, result: Queue, flag: Value, transceiver:TransceiverInterface, finish:AllProcessIsTerminated.allProcessIsTerminated): # type: ignore
+    def pop(self, data_list):
+        with self.__lock:
+            if data_list:
+                return data_list.pop(0)
+
+    def append(self, data_list, item):
+        with self.__lock:
+            data_list.append(item)
+
+    def run(self, flag):
+        data = self.__data
+        result = self.__result
+        loger = Loger(self.__name+"-thread")
+        loger("start") # loger
+        timer = Timer() # timer
+        while True:
+            if flag.value == PROCESSOR_PAUSE:
+                time.sleep(0.1)
+            elif flag.value == PROCESSOR_STOP:
+                break
+            else:
+                if len(data) > 0:
+                    dt = self.pop(data)
+                    timer.start() # timer
+                    self.append(result, self.processing(dt))
+                    timer.end() # timer
+                    loger(dt.index, "데이터 연산 완료", option=timer) # loger
+
+    def __call__(self, data: Queue, result: Queue, flag: Value, transceiver:TransceiverInterface): # type: ignore
         loger = Loger(self.__name) # logger
+        buffer = self.__data
+        bufSize = self.__bufSize
+        resultBuffer = self.__result
         try:
             self.__prepare__()
 
+            th = Thread(target=self.run, args=(flag,))
+            th.start()
+
             timer = Timer() # timer
-            loger(option="start") # loger
+            loger("start") # loger
             while True:
                 if flag.value == PROCESSOR_PAUSE:
                     time.sleep(0.1)
                 elif flag.value == PROCESSOR_STOP:
                     break
                 else:
-                    if not data.empty():
+                    if not data.empty() and len(buffer) < bufSize:
                         timer.start() # timer
                         ret, data_ = transceiver.receive(data)
                         timer.end() # timer
                         loger("데이터 수신 후 압축 해제", option=timer) # loger
                         if ret:
-                            timer.start() # timer
-                            temp = self.processing(data_)
-                            timer.end() # timer
-                            loger(temp.index, "데이터 연산 완료", option=timer) # loger
-                            timer.start() # timer
-                            transceiver.send(result, temp)
-                            timer.end() # timer
-                            loger("데이터 압축 후 전송", option=timer) # loger
-                        elif finish():
-                            break
-                    time.sleep(0.1)
+                            self.append(buffer, data_)
+                    if len(resultBuffer) > 0:
+                        timer.start() # timer
+                        transceiver.send(result, self.pop(resultBuffer))
+                        timer.end() # timer
+                        loger("데이터 압축 후 전송", option=timer) # loger
+
             loger(os.getpid(), "연산 프로세서 종료", option="terminate")
         except Exception as e:
+            flag.value = PROCESSOR_STOP
             loger(os.getpid(), "연산 프로세서 오류", e)
+        th.join(1)
         return
