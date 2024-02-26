c030ed10863f46f8b13725294e84a6e4c066822c
SangJung <sangjung245@gmail.com>
2024-02-26 02:00:40 +0000

    [MODIFY] 파라미터 변경 및 저장 및 불러오기 방법 변경

--- video/model/Frame.py
+++ video/model/Frame.py
@@ -1,20 +1,24 @@
-import cv2
 import numpy as np
 
 from project_constants import FRAME_SCALE, FRAME_FRAME
 
 class Frame:
-    def __init__(self, index:int, frame:np.ndarray, width:int, height:int, rWidth:int, rHeight:int, scale:int, isDetect:bool, filter: object = None):
+    def __init__(self, index:int, frame:np.ndarray, isDetect:bool):
         self.__index = index
-        self.__width = width
-        self.__height = height
-        self.__rWidth = rWidth
-        self.__rHeight = rHeight
-        self.__scale = scale
         self.__isDetect = isDetect
-        self.__filter = filter
         self.__imgs = {FRAME_FRAME: frame}
         self.__face = []
+        self.__isNewScene = False
+
+    @property
+    def isNewScene(self):
+        return self.__isNewScene
+    @isNewScene.setter
+    def isNewScene(self, value):
+        if isinstance(value, bool):
+            self.__isNewScene = value
+        else:
+            raise ValueError("isNewScene is must be bool")
 
     @property
     def face(self):
@@ -25,11 +29,9 @@ class Frame:
             self.__face = value
         else:
             raise ValueError("face is must be list")
-
     @property
     def imgs(self):
         return self.__imgs
-
     @property
     def frame(self):
         return self.__imgs[FRAME_FRAME]
@@ -45,17 +47,16 @@ class Frame:
             self.__isDetect = value
         else:
             raise ValueError("isDetect is must be bool")
-    @property
-    def scale(self):
-        return self.__scale
+        
+    def setFrame(self, value:np.ndarray, cv2Constant = FRAME_SCALE) -> None:
+        if isinstance(value, np.ndarray):
+            self.__imgs[cv2Constant] = value
+            return
+        raise Exception("value는 이미지이어야 함")
 
     # cv2 COLOR 상수 와야함
     def getFrame(self, cv2Constant = FRAME_SCALE) -> np.ndarray:
         if cv2Constant in self.__imgs:
             return self.__imgs[cv2Constant]
-        if cv2Constant == FRAME_SCALE:
-            self.__imgs[cv2Constant] = self.__filter(cv2.resize(self.frame, (self.__rWidth, self.__rHeight))) if self.__filter is not None else cv2.resize(self.frame, (self.__rWidth, self.__rHeight))
-        else:
-            self.__imgs[cv2Constant] = cv2.cvtColor(self.getFrame(), cv2Constant) 
-        return self.__imgs[cv2Constant]
+        raise Exception("이미지 변환 안됨")
     
