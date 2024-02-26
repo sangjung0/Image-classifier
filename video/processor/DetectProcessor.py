be585f6c9aae92374431db3569e99562aae38811
SangJung <sangjung245@gmail.com>
2024-02-26 02:01:55 +0000

    [UPDATE] 파라미터 수정

--- video/processor/DetectProcessor.py
+++ video/processor/DetectProcessor.py
@@ -5,10 +5,11 @@ from face_detector import DetectorInterface
 from video.processor.ProcessorInterface import ProcessorInterface
 
 class DetectProcessor(ProcessorInterface):
-    def __init__(self, detector: Type[DetectorInterface], draw:bool):
+    def __init__(self, detector: Type[DetectorInterface], scale:int, draw:bool):
         super().__init__("DetectProcessor")
         self.__detector = detector
         self.__draw = draw
+        self.__scale = scale
 
     def __prepare__(self):
         if self.__detector is not None: self.__detector = self.__detector()
@@ -18,6 +19,7 @@ class DetectProcessor(ProcessorInterface):
 
         detector = self.__detector
         draw = self.__draw
+        scale = self.__scale 
         frames = []
 
         for frame in section.frames:
@@ -28,7 +30,7 @@ class DetectProcessor(ProcessorInterface):
 
         if len(frames) == 0: return section
 
-        faceLocation = detector.extract( [frame.scale for frame in frames], draw, [frame.frame for frame in frames])
+        faceLocation = detector.extract( scale, draw, [frame.frame for frame in frames])
         detector.clear()
 
         for frame, face in zip(frames, faceLocation):
