faf5b0bd1f1500474f907f1fb3272afeec2e544d
SangJung <sangjung245@gmail.com>
2024-02-26 02:04:08 +0000

    [UPDATE] 이미지 처리 기능 추가 및 트래커 제거

--- video/processor/VisionProcessor.py
+++ video/processor/VisionProcessor.py
@@ -1,35 +1,45 @@
 from typing import Type
+import cv2
 
 from video.model import Section
-from face_tracker import TrackerInterface
 from video.processor.ProcessorInterface import ProcessorInterface
 
 class VisionProcessor(ProcessorInterface):
-    def __init__(self, tracker: Type[TrackerInterface], sceneDetector: Type[object], draw:bool):
+    def __init__(self, filter:Type[object], sceneDetector: Type[object], width:int, height:int, scale:int, colors:set, draw:bool):
         super().__init__("VisionProcessor")
-        self.__tracker = tracker
-        self.__draw = draw
+        self.__filter = filter
         self.__sceneDetector = sceneDetector
+        self.__width = width // scale
+        self.__height = height // scale
+        self.__colors = colors
+        self.__draw = draw
 
     def __prepare__(self):
-        if self.__tracker is not None: self.__tracker = self.__tracker()
+        if self.__filter is not None: self.__filter = self.__filter()
         if self.__sceneDetector is not None: self.__sceneDetector = self.__sceneDetector()
-
+        
     def processing(self, section:Section):
 
+        filter = self.__filter
         sceneDetector = self.__sceneDetector
-        tracker = self.__tracker
+        width = self.__width
+        height = self.__height
         draw = self.__draw
-        isNewScene = False
-
-        if sceneDetector is None and tracker is None:
-            return section
+        colors = self.__colors
 
         for frame in section:
+            f = frame.frame
+            if draw:
+                cf = filter(f) if filter is not None else f
+                f[:] = cf
+                frame.setFrame(cv2.resize(cf,(width, height)))
+            else: frame.setFrame(filter(cv2.resize(f, (width, height))) if filter is not None else cv2.resize(f,width,height))
+            for c in colors:
+                frame.setFrame(cv2.cvtColor(frame.getFrame(), c), cv2Constant = c)
             if sceneDetector is not None:
                 isNewScene = sceneDetector.isNewScene(frame.getFrame())
-                frame.isDetect(isNewScene)
-            if tracker is not None:
-                trackingData = tracker.tracking(frame.getFrame(self.__tracker.colorConstant), frame.scale, isNewScene, draw, frame.frame)
+                if isNewScene : frame.isDetect = isNewScene
+                frame.isNewScene = isNewScene
             section.compress(frame)
+
         return section
