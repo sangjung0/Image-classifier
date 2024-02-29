a424b53ed2694429f387f292c2af350410404d8c
SangJung <sangjung245@gmail.com>
2024-02-29 15:51:45 +0000

    [UPDATE] 구조 변경에 따른 코드 수정

--- video/processor/SinglePoint.py
+++ video/processor/SinglePoint.py
@@ -2,17 +2,15 @@ import os
 
 from video.model import Section
 from video.logic import StartPointInterface
-from util import CompressorInterface
 from util.util import Timer, Loger
 
 class SinglePoint:
-    def __init__(self, name: str, logic:StartPointInterface, compressor = CompressorInterface):
+    def __init__(self, name: str, logic:StartPointInterface):
         self.__loger = Loger(name) # logger
         self.__timer = Timer() # timer
         self.__isFinish = False
         self.__logic = logic
-        self.__logic.prepare()
-        self.__compressor = compressor
+        self.__logic.prepare(self.setIsFinish)
         self.__index = 0
         self.__loger("start") # loger
 
@@ -30,13 +28,10 @@ class SinglePoint:
                 loger(os.getpid(), "종료", option="terminate")
                 return None
 
-            data = timer.measure(self.__logic())
+            data = timer.measure(lambda :self.__logic.processing(Section(self.__index, [])))
             loger("데이터 연산", option=timer)
-            self.__index = data.index
-            videoSection = Section(self.__index, compressor=self.__compressor)
-            for i in data:
-                videoSection.append(i)
-            return videoSection
+            self.__index += 1
+            return data
         except Exception as e:
             loger(os.getpid(), "오류", e)
         loger(f"연산 평균 속도 {timer.average}", option='result')
