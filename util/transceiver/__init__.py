a429b990fcda9590e810397da45a12db4aa06dd0
SangJung <sangjung245@gmail.com>
2024-02-29 05:10:50 +0000

    [ADD] 직렬화와 압축을 제거한 전송기 추가

--- util/transceiver/__init__.py
+++ util/transceiver/__init__.py
@@ -5,6 +5,7 @@
 
 from util.transceiver.TransceiverInterface import TransceiverInterface
 from util.transceiver.Transceiver import Transceiver
+from util.transceiver.EmptyTransceiver import EmptyTransceiver
 
-__all__ = ['TransceiverInterface', 'Transceiver']
+__all__ = ['TransceiverInterface', 'Transceiver', 'EmptyTransceiver']
 __version__ = '0.1'
