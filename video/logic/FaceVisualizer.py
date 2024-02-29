f435fe46b5356ac5182eb816fda3c3d131e73e0b
SangJung <sangjung245@gmail.com>
2024-02-29 09:38:45 +0000

    [RENAME] DetectorVisualizer 에서 FaceVisualizer로 이름 변경

--- video/logic/FaceVisualizer.py
+++ video/logic/FaceVisualizer.py
@@ -6,7 +6,7 @@ from video.logic import StopOverPointInterface
 
 from project_constants import DETECTOR_FACE, DETECTOR_NOSE,DETECTOR_RIGHT_MOUTH, DETECTOR_LEFT_EYE, DETECTOR_LEFT_MOUTH, DETECTOR_RIGHT_EYE,  DETECTOR_FRONT_FACE
 
-class DetectorVisualizer(StopOverPointInterface):
+class FaceVisualizer(StopOverPointInterface):
     __COLORS = {
         DETECTOR_FRONT_FACE : (0, 255, 255),
         DETECTOR_FACE : (255, 0, 0),
@@ -26,25 +26,17 @@ class DetectorVisualizer(StopOverPointInterface):
         DETECTOR_RIGHT_EYE : 3
     }
 
-    def __init__(self, scale:int):
-        self.__scale = scale
-
     def prepare(self) -> None: 
         return
 
     def draw(self, locations: list[Face], frame: np.ndarray):
-        scale = self.__scale
         for face in locations:
             for key in face.faceData:
                 lx, ly, rx, ry = face.faceData[key]
-                lx *= scale
-                ly *= scale
-                rx *= scale
-                ry *= scale
                 cv2.rectangle(
                     frame, 
                     (lx, ly), (rx, ry), 
-                    DetectorVisualizer.__COLORS[key], DetectorVisualizer.__WEIGHT[key]
+                    FaceVisualizer.__COLORS[key], FaceVisualizer.__WEIGHT[key]
                     )
     
     def processing(self, section: Section) -> Section:
