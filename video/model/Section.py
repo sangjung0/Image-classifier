2e8e0d6ed7e66e12172cf5ffd07fc1390ecff1bf
SangJung <sangjung245@gmail.com>
2024-02-29 08:18:53 +0000

    [UPDATE] 이미지 압축 로직과 추가 로직 분리

--- video/model/Section.py
+++ video/model/Section.py
@@ -1,5 +1,6 @@
 from video.model.Frame import Frame
 from util.compressor import CompressorInterface
+
 class Section:
     def __init__(self, index:int, frames:list = [], compressor:CompressorInterface = None):
         self.__index = index
@@ -19,26 +20,25 @@ class Section:
         self.__frames.clear()
     
     def append(self, frame:Frame):
-        self.compress(frame)
         self.__frames.append(frame)
 
-    def compress(self, frame:Frame):
+    def compress(self):
         if self.__compressor is not None:
-            imgs = frame.imgs
-            keys = imgs.keys()
-            for key in keys:
-                if isinstance(imgs[key], bytes):
-                    raise Exception("This frame is already compressed")
-                imgs[key] = self.__compressor.compress(imgs[key])
+            for f in self.__frames:
+                imgs = f.imgs
+                for key in imgs:
+                    if isinstance(imgs[key], bytes):
+                        raise Exception("This frame is already compressed")
+                    imgs[key] = self.__compressor.compress(imgs[key])
 
-    def deCompress(self, frame:Frame):
+    def deCompress(self):
         if self.__compressor is not None:
-            imgs = frame.imgs
-            keys = imgs.keys()
-            for key in keys:
-                if not isinstance(imgs[key], bytes):
-                    raise Exception("This frame is already deCompressed")
-                imgs[key] = self.__compressor.decompress(imgs[key])
+            for f in self.__frames:
+                imgs = f.imgs
+                for key in imgs:
+                    if not isinstance(imgs[key], bytes):
+                        raise Exception("This frame is already deCompressed")
+                    imgs[key] = self.__compressor.decompress(imgs[key])
     
     def __lt__(self, other):
         return self.index < other.index
@@ -47,14 +47,4 @@ class Section:
         return len(self.__frames)
     
     def __iter__(self):
-        return Section.__SectionIter(self.__frames, self.deCompress)
-    
-    class __SectionIter:
-        def __init__(self, frames:list, deCompressor:CompressorInterface):
-            self.__framesIter = iter(frames)
-            self.__deCompressor = deCompressor
-        
-        def __next__(self):
-            frame = next(self.__framesIter)
-            self.__deCompressor(frame)
-            return frame
+        return iter(self.__frames)
