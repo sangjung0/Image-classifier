54511bd16ef0fc01345a090a668e54bb75434f58
SangJung <sangjung245@gmail.com>
2024-02-29 05:15:22 +0000

    [UPDATE] 멀티프로세서 부분과 주요 로직 분리

--- video/logic/Vision.py
+++ video/logic/Vision.py
@@ -2,13 +2,11 @@ from typing import Type
 import cv2
 
 from video.model import Section
-from video.processor.ProcessorInterface import ProcessorInterface
+from video.logic.StopOverPointInterface import StopOverPointInterface
 
-class VisionProcessor(ProcessorInterface):
-    def __init__(self, filter:Type[object], sceneDetector: Type[object], width:int, height:int, scale:int, colors:set, draw:bool):
-        super().__init__("VisionProcessor")
+class Vision(StopOverPointInterface):
+    def __init__(self, filter:Type[object], width:int, height:int, scale:int, colors:set, draw:bool):
         self.__filter = filter
-        self.__sceneDetector = sceneDetector
         self.__width = width 
         self.__height = height 
         self.__rWidth = width // scale
@@ -16,14 +14,12 @@ class VisionProcessor(ProcessorInterface):
         self.__colors = colors
         self.__draw = draw
 
-    def __prepare__(self):
+    def prepare(self) -> None:
         if self.__filter is not None: self.__filter = self.__filter()
-        if self.__sceneDetector is not None: self.__sceneDetector = self.__sceneDetector()
         
-    def processing(self, section:Section):
+    def processing(self, section:Section) -> Section:
 
         filter = self.__filter
-        sceneDetector = self.__sceneDetector
         width = self.__width
         height = self.__height
         rWidth = self.__rWidth
@@ -39,10 +35,6 @@ class VisionProcessor(ProcessorInterface):
             frame.setFrame(cf)
             for c in colors:
                 frame.setFrame(cv2.cvtColor(frame.getFrame(), c), cv2Constant = c)
-            if sceneDetector is not None:
-                isNewScene = sceneDetector.isNewScene(frame.getFrame())
-                if isNewScene : frame.isDetect = isNewScene
-                frame.isNewScene = isNewScene
             section.compress(frame)
 
         return section
