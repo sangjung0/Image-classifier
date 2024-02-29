380b3b3beed38b4ee6f768706a168739e64f8d27
SangJung <sangjung245@gmail.com>
2024-02-29 05:13:48 +0000

    [UPDATE] 분배기 멀티 프로세서 구현 부분과 주요 로직 분리

---
+++ video/logic/Distributor.py
@@ -0,0 +1,39 @@
+from typing import Callable
+
+from video.model import Frame, Section
+from video.VideoData import VideoData
+from video.logic.StartPointInterface import StartPointInterface
+
+class Distributor(StartPointInterface):
+    def __init__(self, videoData:VideoData, detectFrameCount:int, cfl:int):
+        self.__videoData = videoData
+        self.__cfl = cfl
+        self.__detectFrameCount = detectFrameCount
+        self.__iter = None
+        self.__setIsFinish = None
+        self.__index = 0
+
+    def prepare(self, setIsFinish:Callable) -> None:
+        self.__isDetect = lambda x: x % self.__detectFrameCount == 0
+        self.__iter = self.__videoData.__enter__()
+        self.__setIsFinish = setIsFinish
+
+    def processing(self) -> Section:
+        it = self.__iter
+        section = Section(self.__index, [])
+        self.__index += 1
+        isDetect = self.__isDetect
+        cfl = self.__cfl
+        try:
+            while True:
+                index, frame = next(it)
+                section.append(Frame(index, frame, isDetect(index)))
+                if len(section) >= cfl:
+                    break
+        except StopIteration:
+            self.__setIsFinish(True)
+            self.__videoData.__exit__()
+        except Exception as e:
+            self.__videoData.__exit__()
+            raise e
+        return section
