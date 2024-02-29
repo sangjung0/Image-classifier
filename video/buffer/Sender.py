a524ae69c0247e1541fecc026a47d0bfdfc79648
SangJung <sangjung245@gmail.com>
2024-02-29 08:20:23 +0000

    [FEAT] 데이터 전송 전 압축

--- video/buffer/Sender.py
+++ video/buffer/Sender.py
@@ -15,6 +15,7 @@ class Sender(Interface):
     def __call__(self, order:int, terminationSignal:Value, flag:Value, outputQ:Queue, transceiver:TransceiverInterface) -> None: # type: ignore
         loger = Loger(self.__name, self.__logerIsPrint) # loger
         timer = Timer() # timer
+        imgTimer = Timer() # timer
         loger("start", option="start") # loger
         try:
             while True:
@@ -28,12 +29,16 @@ class Sender(Interface):
                             break
                         time.sleep(0.01)
                     else:
-                        timer.measure(lambda :transceiver.send(outputQ, self.get()))
+                        data = self.get()
+                        imgTimer.measure(lambda: data.compress())
+                        loger("이미지 압축", option=imgTimer)
+                        timer.measure(lambda :transceiver.send(outputQ, data))
                         loger("데이터 압축 후 송신", option=timer)
         except Exception as e:
             loger("쓰레드 오류",e, option="error") # loger
             flag.value = PROCESSOR_STOP
         loger("쓰레드 종료", option='terminate') # loger
         loger(f"압축 후 송신 평균 속도 {timer.average}", option='result')
+        loger(f"이미지 압축 평균 속도 {imgTimer.average}", option='result')
         return
 
