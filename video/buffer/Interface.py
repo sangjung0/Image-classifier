5de1597fe3f159ab9a354137191997675ac7aa5f
SangJung <sangjung245@gmail.com>
2024-03-02 14:37:38 +0000

    [FEAT] 버퍼 내부에 순차적으로 데이터 받는 기능 추가

--- video/buffer/Interface.py
+++ video/buffer/Interface.py
@@ -5,27 +5,41 @@ from video.model import Section
 
 class Interface:
     def __init__(self, requiresSorting:bool = False) -> None:
-        self._datas = []
+        self.__datas = []
         self.__lock = Lock()
+        self.__index = 0
+
         self.__append = self.__sortAppend if requiresSorting else self.__unSortAppend
+        self.__get = self.__sortGet if requiresSorting else self.__unSortGet
 
     def __sortAppend(self, value:Section) -> None:
-        bisect.insort(self._datas, value)
+        bisect.insort(self.__datas, value)
     
     def __unSortAppend(self, value:Section) -> None:
-        self._datas.append(value)
+        self.__datas.append(value)
+
+    def __sortGet(self) -> Section:
+        if self.__datas[0].index == self.__index:
+            self.__index += 1
+            return self.__datas.pop(0)
+        return None
+        
+    def __unSortGet(self) -> Section:
+        return self.__datas.pop(0)
+    
+    def __len__(self) -> int:
+        return len(self.__datas)
 
     def append(self, value:Section) -> None:
         with self.__lock:
             self.__append(value)
 
-    def get(self, index = None) -> Section:
+    def get(self) -> Section:
         with self.__lock:
-            if not self.empty() and (index == None or self._datas[0].index == index):
-                return self._datas.pop(0)
-            return None
+            if not self.empty(): return self.__get()
+        return None
     
     def empty(self)->bool:
-        return len(self._datas) == 0
+        return len(self.__datas) == 0
     
 
