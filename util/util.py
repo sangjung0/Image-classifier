d7ad742781f51cd49ead362c36258743a233bd1b
SangJung <sangjung245@gmail.com>
2024-03-03 10:18:52 +0000

    [MODIFY] 로거 공백 길이 수정

--- util/util.py
+++ util/util.py
@@ -56,7 +56,7 @@ class Loger:
 
     def __call__(self,*msg, option:object = None):
         if self.__isPrint:
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] - {self.__name.ljust(20)} - {str(option if option else None).ljust(20)}: ", " ".join(str(i) for i in msg))
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] - {self.__name.ljust(30)} - {str(option if option else None).ljust(20)}: ", " ".join(str(i) for i in msg))
 
 
 def showImages(ary, ratio=1):
