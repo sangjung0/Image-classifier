3e75ac3e5018860bbc9a7fd3606c1c67b42fc403
SangJung <sangjung245@gmail.com>
2024-03-03 12:05:28 +0000

    [ADD] 얼굴 이미지 추출 클래스 추가

--- test/ExtractFace.py
+++ test/ExtractFace.py
@@ -5,28 +5,23 @@ from project_constants import PROCESSOR_STOP
 from video import Loader
 from util.util import Loger
 
-class SaveImg:
+class ExtractFace:
     def __init__(self, videoLoader:Loader):
         self.__videoLoader = videoLoader
 
-    def save(self):
+    def extract(self):
         self.__videoLoader.run()
-        it = iter(self.__videoLoader)
-        loger = Loger("SaveImg")
+        loger = Loger("ExtractFace")
         loger("start")
+
         idx = 0
         all_images = []
-        while True:
-            flag, ret, frame = next(it)
-            if idx >= 20000: break
+        for flag, ret, frame in self.__videoLoader:
             if ret:
                 if idx % 100 == 0:
                     loger(f"{idx} 완료")
-                for lx, ly, rx, ry in frame.face:
-                    if lx < 0: lx = 0
-                    if ly < 0: ly = 0
-                    if rx < 0: rx = 0
-                    if ry < 0: ry = 0
+                for face in frame.face:
+                    lx, ly, rx, ry = face.face
                     face = frame.frame[ly:ry, lx:rx]
                     width = rx - lx
                     height = ry - ly
@@ -41,21 +36,10 @@ class SaveImg:
                     newFace = np.zeros((50, 50, 3), dtype=np.uint8)
                     newFace[(50-height)//2:(50-height)//2+height, (50-width)//2:(50-width)//2+width] = face
 
-                    cv2.imshow("tt",newFace)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        cv2.destroyAllWindows()
-                        all_images = np.array(all_images, dtype=np.uint8)
-                        self.__videoLoader.stop()
-                        np.save("test.npz", all_images)
-                        return
-
                     all_images.append(newFace)
-
                 idx+=1
             elif flag == PROCESSOR_STOP:
                 break
-        cv2.destroyAllWindows()
+        loger(f"완료. {len(all_images)}",  option="terminate")
         all_images = np.array(all_images, dtype=np.uint8)
-        self.__videoLoader.stop()
-        np.save("test", all_images)
-        return
+        return all_images
--- test/__init__.py
+++ test/__init__.py
@@ -4,8 +4,8 @@ unit 테스트 형시이 아닌, 개발과정에서 사용 될 테스트 코드�
 
 from test import video_test
 from test.ImgTable import ImgTable
-from test.SaveImg import SaveImg, LoadImg
+from test.ExtractFace import ExtractFace
 from test.SaveFace import SaveFace, LoadFace
 
-__all__ = ['video_test', 'ImgTable', 'SaveImg', 'SaveFace','LoadFace','LoadImg']
+__all__ = ['video_test', 'ImgTable', 'ExtractFace', 'SaveFace','LoadFace']
 __version__ = '0.1'
