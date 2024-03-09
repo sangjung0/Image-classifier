468f05b2414cda6303f522ac296144f71970cce6
SangJung <sangjung245@gmail.com>
2024-03-09 16:24:29 +0900

    [FEAT] 이미지에서 메타데이터와 이미지 데이터 파싱 기능 추가

--- process/logic/Distributor.py
+++ process/logic/Distributor.py
@@ -1,44 +1,64 @@
+from PIL import Image as Img
+from PIL.ExifTags import GPSTAGS
+from pathlib import Path
+from typing import Iterable
+import numpy as np
+import cv2
+from datetime import datetime as dt
+
 from process.logic.StartPointInterface import StartPointInterface
 
-from process.model import Frame, Section
-from process.VideoData import VideoData
+from process.model import Image, Section
+
+GPS = 34856
+DATETIME = 306
 
 class Distributor(StartPointInterface):
-    def __init__(self, videoData:VideoData, detectFrameCount:int, cfl:int):
-        self.__videoData = videoData
+    def __init__(self, pathData:Iterable[Path], cfl:int):
+        self.__pathData = pathData
         self.__cfl = cfl
-        self.__detectFrameCount = detectFrameCount
         self.__iter = None
         self.__setIsFinish = None
-        self.__index = 0
 
     def prepare(self, setIsFinish:callable) -> None:
-        self.__isDetect = lambda x: x % self.__detectFrameCount == 0
-        self.__iter = self.__videoData.__enter__()
+        self.__iter = iter(self.__pathData)
         self.__setIsFinish = setIsFinish
 
     def processing(self, section:Section) -> Section:
         it = self.__iter
-        self.__index += 1
-        isDetect = self.__isDetect
         cfl = self.__cfl
+
         try:
             while True:
-                index, frame = next(it)
-                section.append(Frame(index, frame, isDetect(index % cfl)))
+                path = next(it)
+                img, gps, datetime = self.getInfo(path)
+                print(gps, datetime)
+                section.append(Image(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), path))
                 if len(section) >= cfl:
                     break
         except StopIteration:
             self.__setIsFinish(True)
-            self.__videoData.__exit__()
         except Exception as e:
-            self.__videoData.__exit__()
             raise e
         return section
-
-class DetectFrame:
-    def __init__(self, detectFrameCount):
-        self.__detectFrameCount = detectFrameCount
     
-    def isDetect(self, x):
-        return x % self.__detectFrameCount == 0
+    def getInfo(self, path:Path):
+        img = Img.open(str(path))
+        exif = img.getexif()
+        if not exif:
+            return np.array(img), None, None
+        
+        gps = None
+        if GPS in exif:
+            gps = {}
+            for t, value in GPSTAGS.items():
+                if t in exif[GPS]:
+                    gps[value] = exif[GPS][t]
+        
+        datetime = None
+        if DATETIME in exif:
+            datetime = dt.strptime(exif[DATETIME],"%Y:%m:%d %H:%M:%S")
+
+        return np.array(img), gps, datetime
+    
+        
