3b5c05eb60395b0a585c232c1c7c84fb3b912b30
SangJung <sangjung245@gmail.com>
2024-03-09 16:49:34 +0900

    [TEST] 테스트 코드

--- test/__init__.py
+++ test/__init__.py
@@ -2,10 +2,7 @@
 unit 테스트 형시이 아닌, 개발과정에서 사용 될 테스트 코드들의 패키지
 """
 
-from test import video_test
-from test.ImgTable import ImgTable
-from test.ExtractFace import ExtractFace
-from test.SaveFace import SaveFace, LoadFace
+from test import test
 
-__all__ = ['video_test', 'ImgTable', 'ExtractFace', 'SaveFace','LoadFace']
+__all__ = ['test']
 __version__ = '0.1'
