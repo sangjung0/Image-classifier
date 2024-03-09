76e9b669e6181ec016787c820d7036299f56fac3
SangJung <sangjung245@gmail.com>
2024-03-09 16:25:32 +0900

    [UPDATE] 변수이름 변경

--- process/logic/FaceVisualizer.py
+++ process/logic/FaceVisualizer.py
@@ -18,38 +18,38 @@ class FaceVisualizer(StopOverPointInterface):
         DETECTOR_RIGHT_EYE : (255,255,0)
     }
     __WEIGHT = {
-        DETECTOR_FRONT_FACE : 4,
-        DETECTOR_FACE : 3,
-        DETECTOR_NOSE : 3,
-        DETECTOR_RIGHT_MOUTH : 3,
-        DETECTOR_LEFT_MOUTH : 3,
-        DETECTOR_LEFT_EYE : 3,
-        DETECTOR_RIGHT_EYE : 3
+        DETECTOR_FRONT_FACE : 40,
+        DETECTOR_FACE : 40,
+        DETECTOR_NOSE : 40,
+        DETECTOR_RIGHT_MOUTH : 40,
+        DETECTOR_LEFT_MOUTH : 40,
+        DETECTOR_LEFT_EYE : 40,
+        DETECTOR_RIGHT_EYE : 40
     }
 
     def prepare(self) -> None: 
         return
 
-    def draw(self, locations: list[Face], frame: np.ndarray):
+    def draw(self, locations: list[Face], source: np.ndarray):
         for face in locations:
             for key in face.faceData:
                 lx, ly, rx, ry = face.faceData[key]
                 cv2.rectangle(
-                    frame, 
+                    source, 
                     (lx, ly), (rx, ry), 
                     face.rectColor if key == DETECTOR_FACE else FaceVisualizer.__COLORS[key], FaceVisualizer.__WEIGHT[key]
                     )
                 if key == DETECTOR_FACE:
-                    name = face.name.get()
+                    name = face.name
                     if name:
-                        cv2.rectangle(frame, (lx, ly), (lx+30, ly-30), face.rectColor, cv2.FILLED)
-                        cv2.putText(frame, name, (lx, ly),  cv2.FONT_HERSHEY_SIMPLEX, 1, tuple((255 - i) for i in face.rectColor), 4)
+                        cv2.rectangle(source, (lx, ly), (lx+30, ly-30), face.rectColor, cv2.FILLED)
+                        cv2.putText(source, name, (lx, ly),  cv2.FONT_HERSHEY_SIMPLEX, 1, tuple((255 - i) for i in face.rectColor), 4)
     
     def processing(self, section: Section) -> Section:
 
-        for frame in section:
-            if len(frame.face) > 0:
-                self.draw(frame.face, frame.frame)
+        for img in section:
+            if len(img.face) > 0:
+                self.draw(img.face, img.source)
 
         return section
 
