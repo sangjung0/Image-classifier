34cb66bf8fcde6873a39fa25c5bb7d30ea41b989
SangJung <sangjung245@gmail.com>
2024-03-09 16:28:03 +0900

    [UPDATE] 이미지 처리로 변경되어 연속성을 가지는 데이터 제거

--- process/model/Face.py
+++ process/model/Face.py
@@ -1,69 +1,39 @@
 import random
 
-from process.model.Name import Name
-
 from project_constants import DETECTOR_FACE, DETECTOR_NOSE,DETECTOR_RIGHT_MOUTH, DETECTOR_LEFT_EYE, DETECTOR_LEFT_MOUTH, DETECTOR_RIGHT_EYE,  DETECTOR_FRONT_FACE
 
 class Face:
-    def __init__(self, frameIndex, faceData):
-        self.__frameIndex = frameIndex
+    def __init__(self, faceData:dict[str:tuple[int,int,int,int]]) -> None:
         self.__faceData = faceData
         self.__rectColor = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-        self.__points = []
-        self.__nextFace = None
-        self.__name = Name()
+        self.__name = None
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @property
-    def points(self):
-        return self.__points
-    @points.setter
-    def points(self, value):
-        if isinstance(value, list):
-            self.__points = value
-        else:
-            raise ValueError("points is must be list")
-    @property
-    def rectColor(self):
+    def rectColor(self) -> tuple[int, int, int]:
         return self.__rectColor 
     @property
-    def frameIndex(self):
-        return self.__frameIndex
-    @property
-    def faceData(self):
+    def faceData(self) -> dict[str:tuple[int,int,int,int]]:
         return self.__faceData
     @property
-    def face(self):
+    def face(self) -> tuple[int,int,int,int]:
         return self.__faceData[DETECTOR_FACE]
     @property
-    def nose(self):
+    def nose(self) -> tuple[int,int,int,int]:
         return self.__faceData.get(DETECTOR_NOSE,None)
     @property
-    def lEye(self):
+    def lEye(self) -> tuple[int,int,int,int]:
         return self.__faceData.get(DETECTOR_LEFT_EYE, None)
     @property
-    def rEye(self):
+    def rEye(self) -> tuple[int,int,int,int]:
         return self.__faceData.get(DETECTOR_RIGHT_EYE, None)
     @property
-    def lMouth(self):
+    def lMouth(self) -> tuple[int,int,int,int]:
         return self.__faceData.get(DETECTOR_LEFT_MOUTH, None)
     @property
-    def rMouth(self):
+    def rMouth(self) -> tuple[int,int,int,int]:
         return self.__faceData.get(DETECTOR_RIGHT_MOUTH, None)
-    @property
-    def nextFace(self):
-        return self.__nextFace
-
-    @nextFace.setter
-    def nextFace(self, value):
-        if self.__nextFace is not None : raise Exception("nextFace is not None")
-        if isinstance(value, Face):
-            self.__nextFace = value
-            value.__rectColor = self.__rectColor
-            value.__name = self.__name
-        else:
-            raise ValueError("nextFace is must be Face")
     
