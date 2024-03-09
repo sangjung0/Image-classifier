296a94dda0040122bcbeefd17950ec194f016e57
SangJung <sangjung245@gmail.com>
2024-03-09 16:32:00 +0900

    [UPDATE] 이미지 처리로 변경됨에 따라 연속된 데이터를 처리하는 프로세서 제거

--- process/Controller.py
+++ process/Controller.py
@@ -1,97 +1,66 @@
 from typing import Type
 
-from process.face_detector import DetectorInterface
-from process.tracker import TrackerInterface
-from process.VideoData import VideoData
 from process.logic import *
 from process.processor import Controller as PC
 from process.Loader import Loader, SingleLoader
 from process.compressor import CompressorInterface, UnCompressor
 from process.serializer import PickleSerializer
 from process.transceiver import TransceiverInterface, Transceiver
-from process.scene_detector import Interface
-from process.model import Face
+from process.PathData import PathData
+from process.face_detector import DetectorInterface
+
 
 class Controller:
 
     @staticmethod
-    def playSingleFromFaces(fileName:str, faces:list[Face], cfl:int= 200, draw:bool = False):
-        videoData = VideoData(fileName)
-        logics = [
-            Distributor(videoData, 1, cfl),
-            InsertFace(faces)
-        ]
-
-        if draw:
-            logics.append(FaceVisualizer())
-
-        process = PC.getSingle("singleProcessor", logics)
-
-        return SingleLoader(videoData, process)
-
-    @staticmethod
     def startSingleAndGetVideoLoader(
-        fileName:str, detectFrameCount:int = 1, scale:int = 1, cfl:int = 200, pointNumber:int = 1000,
-        filter = None, detector:Type[DetectorInterface] = None, tracker:Type[TrackerInterface] = None, sceneDetector:Type[Interface] = None, draw:bool = False
-    ):
-        videoData = VideoData(fileName)
+        path:str, scale:int = 1, cfl:int = 200,
+        filter = None, detector:Type[DetectorInterface] = None, draw:bool = False
+    ) -> SingleLoader:
+        pathData = PathData(path)
         colors = set()
 
         if detector is not None: colors.add(detector.COLOR)
-        if tracker is not None: colors.add(tracker.COLOR)
 
         logics = [
-            Distributor(videoData, detectFrameCount, cfl), 
-            Vision(filter, videoData.width, videoData.height, scale, colors, draw)
+            Distributor(pathData, cfl), 
+            Vision(filter, scale, colors, draw)
         ]
-
-        if sceneDetector is not None:
-            logics.append(SceneDetector(sceneDetector))
 
         if detector is not None:
             logics.append(Detector(detector, scale))
-
-        if tracker is not None:
-            logics.append(Tracker(tracker, scale))
-            logics.append(FaceTracker())
+            logics.append(FaceResizer(0))
 
         if draw:
             logics.append(FaceVisualizer())
-            logics.append(TraceLineVisualizer(pointNumber))
 
 
         process = PC.getSingle("singleProcessor", logics)
 
-        return SingleLoader(videoData, process)
-
+        return SingleLoader(pathData, process)
 
     @staticmethod
     def startAndGetVideoLoader(
-        fileName:str, visionProcessorNumber:int = 1, detectProcessorNumber:int=1,  trackerProcessorNumber:int=1, detectFrameCount:int = 1, scale:int = 1, cfl:int = 200, bufSize:int = 64, pointNumber:int = 1000,
+        path:str, visionProcessorNumber:int = 1, detectProcessorNumber:int=1, scale:int = 1, cfl:int = 200, bufSize:int = 64,
         transceiver:TransceiverInterface = Transceiver(PickleSerializer(), UnCompressor()), compressor: Type[CompressorInterface] = UnCompressor(),
-        filter = None, detector:Type[DetectorInterface] = None, tracker:Type[TrackerInterface] = None, sceneDetector:Type[object] = None, draw:bool = False
-        ):
+        filter = None, detector:Type[DetectorInterface] = None, draw:bool = False
+        ) -> Loader:
 
-        videoData = VideoData(fileName)
+        pathData = PathData(path)
         colors = set()
 
         if detector is not None: colors.add(detector.COLOR)
-        if tracker is not None: colors.add(tracker.COLOR)
         
         logics = [
             [
-                Distributor(videoData, detectFrameCount, cfl), 
-                Vision(filter, videoData.width, videoData.height, scale, colors, draw),
-                Tracker(tracker,scale)
+                Distributor(pathData, cfl), 
+                Vision(filter, scale, colors, draw)
             ]
         ]
         pNumbers = []
         pBufSize = [bufSize]
         bufSort = []
         names = ["StartProcess"]
-
-        if sceneDetector is not None:
-            logics[0].append(SceneDetector(sceneDetector))
 
         if detector is not None:
             logics.append([
@@ -102,21 +71,10 @@ class Controller:
             pBufSize.append(3)
             bufSort.append(True)
 
-        # if tracker is not None:
-        #     logics.append([
-        #         Tracker(tracker, scale),
-        #         FaceTracker()
-        #     ])
-        #     names.append("TrackerProcess")
-        #     pNumbers.append(trackerProcessorNumber)
-        #     pBufSize.append(3)
-
         if draw:
             logics.append([
-                FaceTracker(),
-                FaceFilter(videoData.width, videoData.height, 0),
-                FaceVisualizer(),
-                TraceLineVisualizer(pointNumber)
+                #FaceResizer(0),
+                FaceVisualizer()
             ])
             names.append("Drawers")
             pNumbers.append(1)
@@ -131,4 +89,4 @@ class Controller:
 
         flag, receiver, receiverTh, allProcess, allTransmissionMedium = PC.get(names, logics, pNumbers, pBufSize, bufSort, compressor, transceiver)
 
-        return Loader(videoData, receiver, receiverTh, flag, allProcess, allTransmissionMedium)
+        return Loader(pathData, receiver, receiverTh, flag, allProcess, allTransmissionMedium)
