52becb4ec30ce7c3f129faf0619907b3a59957ed
SangJung <sangjung245@gmail.com>
2024-03-22 10:45:45 +0000

    [FEAT] transceiver 인터페이스 구현

--- program/image_data/transceiver/Empty.py
+++ program/image_data/transceiver/Empty.py
@@ -1,11 +1,20 @@
+from multiprocessing import Queue as Q
+
 from image_data.transceiver import Interface
+
+from image_data.compressor import CompressorInterface
+from image_data.serializer import SerializerInterface
 
 class Empty(Interface):
 
-    def send(self, destination, value):
-        destination.put(value)
+    def __init__(self, source:Q, destination:Q):
+        self.__source = source
+        self.__destination = destination
 
-    def receive(self, destination):
-        if destination.empty():
+    def send(self, value):
+        self.__destination.put(value)
+
+    def receive(self):
+        if self.__source.empty():
             return False, None
-        return True, destination.get()
+        return True, self.__source.get()
