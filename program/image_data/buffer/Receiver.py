a8688bb78a4ee22d306d88075b08cd5c2b3cae44
SangJung <sangjung245@gmail.com>
2024-03-22 03:41:12 +0000

    [UPDATE] 리시버와 데이터 구조를 분리
    데이터 수신 기능과 데이터를 받는 구조를 분리함
    데이터 받는 구조를 분리함으로써 멀티프로세서간의 데이터 전송이 Queue든 Array든 상관없이 동일하게 사용 가능 함

--- program/image_data/buffer/Receiver.py
+++ program/image_data/buffer/Receiver.py
@@ -1,9 +1,9 @@
-from multiprocessing import Queue, Value
+from multiprocessing.sharedctypes import SynchronizedBase
 import time
 
 from image_data.buffer.Interface import Interface
 
-from image_data.transceiver import TransceiverInterface
+from image_data.transceiver import Interface as TI
 
 from project_constants import PROCESSOR_STOP, PROCESSOR_PAUSE
 
@@ -19,10 +19,9 @@ class Receiver(Interface):
     def isFull(self) -> bool:
         return len(self) == self.__bufSize
 
-    def __call__(self, order:int, terminationSignal:Value, flag:Value, inputQ:Queue, transceiver:TransceiverInterface) -> None: # type: ignore
+    def __call__(self, order:int, terminationSignal:SynchronizedBase, flag:SynchronizedBase, transceiver:TI) -> None: # type: ignore
         loger = Loger(self.__name, self.__logerIsPrint) # loger
         timer = Timer() # timer
-        imgTimer = Timer() # timer
         loger("start", option="start") # loger
         try:
             while True:
@@ -31,23 +30,19 @@ class Receiver(Interface):
                 elif flag.value == PROCESSOR_PAUSE:
                     time.sleep(0.1)
                 else:
-                    if inputQ.empty():
-                        if terminationSignal.value >= order:
-                            break
-                        time.sleep(0.01)
-                    elif self.isFull():
-                        time.sleep(0.01)
-                    else:
-                        ret, data = timer.measure(lambda :transceiver.receive(inputQ))
+                    if not self.isFull():
+                        ret, data = timer.measure(lambda :transceiver.receive())
                         loger("데이터 수신 후 압축 해제", option=timer)
                         if ret: 
-                            imgTimer.measure(lambda: data.deCompress())
                             self.append(data)
+                        elif terminationSignal.value >= order:
+                            break
+                        else:
+                            time.sleep(0.01)
         except Exception as e:
             loger("쓰레드 오류",e, option="error") # loger
             flag.value = PROCESSOR_STOP
         loger(f"데이서 수신 후 압축 해제 평균 속도 {timer.average}", option='result') # loger
-        loger(f"이미지 압축 해제 평균 속도 {imgTimer.average}", option='result') # loger
         loger("쓰레드 종료", option='terminate') # loger
         return
 
