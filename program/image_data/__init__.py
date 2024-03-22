7a8693c2dc0dcaac7034ea65da35d1a0e6f4a944
SangJung <sangjung245@gmail.com>
2024-03-22 03:40:55 +0000

    [RENAME] 이름 변경에 따른 참조 변경

--- program/image_data/__init__.py
+++ program/image_data/__init__.py
@@ -2,10 +2,10 @@
 비디오 재생 패키지
 """
 
-from process.VideoPlayer import VideoPlayer
-from process.Loader import Loader
-from process.Controller import Controller
-from process.PathData import PathData
+from image_data.VideoPlayer import VideoPlayer
+from image_data.Loader import Loader
+from image_data.Controller import Controller
+from image_data.PathData import PathData
 
 __all__ = ['VideoPlayer', 'Controller', 'Loader', 'PathData' ]
 __version__='0.1'
