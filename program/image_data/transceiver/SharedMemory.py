0ac3bf03dccde334c69033b8d28306ef1792f35d
SangJung <sangjung245@gmail.com>
2024-03-22 10:39:59 +0000

    [FEAT] SharedMemory의 transceiver에서 Section 생성
    기존의 SharedMemory의 transceiver은 공유메모리에서 받을 수 있는 데이터를 넘겼지만
    통일성을 지키기위해 transceiver에서 직접 Section을 생성하게 변경

--- program/image_data/transceiver/SharedMemory.py
+++ program/image_data/transceiver/SharedMemory.py
@@ -1,29 +1,46 @@
 from multiprocessing.shared_memory import SharedMemory as SM
 import numpy as np
+import time
 
 from image_data.transceiver.Interface import Interface
 
+from image_data.model import Section, Image
+from project_constants import CHARACTER_SIZE
+
 class SharedMemory(Interface):
-    def __init__(self, imageMemory:SM, imageNumberMemory:SM, keyMemory:SM, prevKey:int,
-                 key:int, nextKey:int, size:tuple[int, int, int, int, int], dtype=np.uint8,):
+    MAX_LOOP = 100
+
+    def __init__(self, batchSize:int, memory:SM, prevKey:int, key:int, nextKey:int, size:tuple[int, int], dtype=np.uint8):
         
         self.size = size
+        self.__imgSizeStart = 1 + CHARACTER_SIZE
         self.__key = key
         self.__prevKey = prevKey
         self.__nextKey = nextKey
+        self.__batchSize = batchSize
         self.__index = 0
+        self.__sectionIdx = 0
 
-        self.imageMemory = np.ndarray(size, dtype=dtype, buffer=imageMemory)
-        self.imageNumberMemory = np.ndarray(size[0:2], dtype=dtype, buffer=imageNumberMemory)
-        self.keyMemory = np.ndarray(size[0:1], dtype=dtype, buffer=keyMemory)
-
+        self.memory = np.ndarray(size, dtype=dtype, buffer=memory)
 
     def send(self, index:int) -> None:
-        self.keyMemory[index] = self.__nextKey
+        self.memory[index][0] = self.__nextKey
 
-    def receive(self):
-        if self.keyMemory[self.__index] == self.__prevKey:
-            self.keyMemory[self.__index] = self.__key
-            if self.keyMemory[self.__index] == self.key:
-                return zip(self.imageNumberMemory[self.__index], self.imageMemory[self.__index])
-        self.__index += 1
+    def receive(self) -> tuple[bool, Section]:
+        l = SharedMemory.MAX_LOOP
+        section = Section(self.__sectionIdx)
+        self.__sectionIdx += 1
+
+        while(l>0 and len(section) < self.__batchSize):
+            if self.memory[self.__index, 0] == self.__prevKey:
+                self.memory[self.__index, 0] = self.__key
+                if self.memory[self.__index, 0] == self.__key: # 이 과정이 필요할까?
+                    section.append(Image(self.__index, self.memory[self.__index, self.__imgSizeStart:]))
+            else:
+                l -= 1
+                time.sleep(0.01)
+            self.__index += 1
+
+        if len(section) > 0:
+            return True, section
+        return False, None
