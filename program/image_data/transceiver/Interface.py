8423dc3c2c895c3b0095c8ee9a84c83fa22a5a35
SangJung <sangjung245@gmail.com>
2024-03-22 10:44:23 +0000

    [UPDATE] transceiver의 구현 방법 변경
    단순히 공간으로부터 데이터를 받아오는 것에서
    특정 데이터 공간을 멤버변수로 갖고 메서드 호출로 데이터를 반환하게 변경

--- program/image_data/transceiver/Interface.py
+++ program/image_data/transceiver/Interface.py
@@ -1,17 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Union
 
-from image_data.compressor import CompressorInterface
-from image_data.serializer import SerializerInterface
-
 class Interface(ABC):
 
-    def __init__(self, serializer:SerializerInterface, compressor: CompressorInterface):
-        self._compressor = compressor
-        self._serializer = serializer
+    @abstractmethod
+    def send(self, value: object) -> None: pass
 
     @abstractmethod
-    def send(self, destination: object, value: object) -> None: pass
-
-    @abstractmethod
-    def receive(self, destination: object) -> Union[bool, object]: pass
+    def receive(self) -> Union[bool, object]: pass
--- program/image_data/transceiver/Queue.py
+++ program/image_data/transceiver/Queue.py
@@ -1,13 +1,23 @@
 from multiprocessing import Queue as Q
 
-from image_data.transceiver import Interface
+from image_data.transceiver.Interface import Interface
+
+from image_data.compressor import CompressorInterface
+from image_data.serializer import SerializerInterface
+from image_data.model import Section
 
 class Queue(Interface):
 
-    def send(self, destination: Q, value: object) -> None:
-        destination.put(self._compressor.compress(self._serializer.serialization(value)))
+    def __init__(self, source:Q, destination:Q, serializer:SerializerInterface, compressor: CompressorInterface):
+        self.__source = source
+        self.__destination = destination
+        self.__compressor = compressor
+        self.__serializer = serializer
+ 
+    def send(self,value: Section) -> None:
+        self.__destination.put(self.__compressor.compress(self.__serializer.serialization(value)))
 
-    def receive(self, destination: Q) -> object:
-        if destination.empty():
+    def receive(self) -> Section:
+        if self.__source.empty():
             return False, None
-        return True, self._serializer.deSerialization(self._compressor.decompress(destination.get()))
+        return True, self.__serializer.deSerialization(self.__compressor.decompress(self.__source.get()))
