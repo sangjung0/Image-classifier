45aa98cad7cc6c09224bc92a63f44757c04569f4
SangJung <sangjung245@gmail.com>
2024-03-23 10:37:59 +0000

    [FEAT] SharedMemory 참조 추가

--- program/image_data/transceiver/__init__.py
+++ program/image_data/transceiver/__init__.py
@@ -6,6 +6,7 @@
 from image_data.transceiver.Interface import Interface
 from image_data.transceiver.Queue import Queue
 from image_data.transceiver.Empty import Empty
+from image_data.transceiver.SharedMemory import SharedMemory
 
-__all__ = ['Interface', 'Queue', 'Empty']
+__all__ = ['Interface', 'Queue', 'Empty', 'SharedMemory']
 __version__ = '0.1'
