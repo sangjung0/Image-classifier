242581a847990a890ed46fb8f72b8f583a204c6a
SangJung <sangjung245@gmail.com>
2024-03-23 10:38:23 +0000

    [MODIFY] send와 receive 큐 분리
    기존에 send와 receive가 각각의 큐로 전달하는 것에서, 동일한 큐로 전달하는 것으로 변경

--- program/image_data/transceiver/Queue.py
+++ program/image_data/transceiver/Queue.py
@@ -2,20 +2,19 @@ from multiprocessing import Queue as Q
 
 from image_data.transceiver.Interface import Interface
 
-from image_data.compressor import CompressorInterface
-from image_data.serializer import SerializerInterface
+from image_data.compressor import Interface as CI
+from image_data.serializer import Interface as SI
 from image_data.model import Section
 
 class Queue(Interface):
 
-    def __init__(self, source:Q, destination:Q, serializer:SerializerInterface, compressor: CompressorInterface):
+    def __init__(self, source:Q, serializer:SI, compressor: CI):
         self.__source = source
-        self.__destination = destination
         self.__compressor = compressor
         self.__serializer = serializer
  
     def send(self,value: Section) -> None:
-        self.__destination.put(self.__compressor.compress(self.__serializer.serialization(value)))
+        self.__source.put(self.__compressor.compress(self.__serializer.serialization(value)))
 
     def receive(self) -> Section:
         if self.__source.empty():
