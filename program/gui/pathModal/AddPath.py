4ce9618c591d4272f998f937d8edf3331a326c31
SangJung <sangjung245@gmail.com>
2024-05-25 04:54:31 +0000

    [FEAT] PathModal 클래스 변경에 따른 매개변수 전달 변경

--- program/gui/pathModal/AddPath.py
+++ program/gui/pathModal/AddPath.py
@@ -1,8 +1,16 @@
-from program.gui.pathModal.PathModal import PathModal
+from PyQt5.QtCore import QPoint
+
+from gui.pathModal.PathModal import PathModal
 
 
 class AddPath(PathModal):
-    __SUCCESS_NAME: str = "add"
+    __CONFIRM_NAME: str = "Add"
+    __CANCEL_NAME: str = "Cancel"
 
-    def __init__(self, callback: callable):
-        super().__init__(callback, AddPath.__SUCCESS_NAME)
+    def __init__(self, center:QPoint, success_callback: callable, cancel_callback: callable):
+        """
+        center -- QPoint 모달 center 위치
+        confirm_callback -- callable 성공시 실행 함수
+        cancel_callback -- callable 실패시 실행 함수
+        """
+        super().__init__(center, success_callback, cancel_callback, AddPath.__CONFIRM_NAME, AddPath.__CANCEL_NAME)
--- program/gui/pathModal/Init.py
+++ program/gui/pathModal/Init.py
@@ -1,8 +1,17 @@
-from program.gui.pathModal.PathModal import PathModal
+from PyQt5.QtCore import QPoint
+
+from gui.pathModal.PathModal import PathModal
 
 
 class Init(PathModal):
-    __SUCCESS_NAME: str = "start"
+    __CONFIRM_NAME: str = "Start"
+    __CANCEL_NAME: str = "Exit"
+    
 
-    def __init__(self, callback: callable):
-        super().__init__(callback, Init.__SUCCESS_NAME)
+    def __init__(self, center:QPoint, success_callback: callable, cancel_callback: callable):
+        """
+        center -- QPoint 모달 center 위치
+        confirm_callback -- callable 성공시 실행 함수
+        cancel_callback -- callable 실패시 실행 함수
+        """
+        super().__init__(center, success_callback, cancel_callback, Init.__CONFIRM_NAME, Init.__CANCEL_NAME)
