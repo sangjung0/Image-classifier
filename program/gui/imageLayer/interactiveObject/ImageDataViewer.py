0d2db1e78013d6fdeff4ac33d04271a92c6cd095
SangJung <sangjung245@gmail.com>
2024-05-25 13:43:02 +0000

    [FEAT] 임시 스타일 추가 및 set_data 함수 구현

--- program/gui/imageLayer/interactiveObject/ImageDataViewer.py
+++ program/gui/imageLayer/interactiveObject/ImageDataViewer.py
@@ -1,7 +1,8 @@
 import datetime
 import pathlib
+from PyQt5.QtWidgets import QWidget
 
-from program.gui.imageLayer.interactiveObject.InteractiveObject import InteractiveObject
+from gui.imageLayer.interactiveObject.InteractiveObject import InteractiveObject
 
 
 class ImageDataViewer(InteractiveObject):
@@ -10,8 +11,27 @@ class ImageDataViewer(InteractiveObject):
     __WIDTH: int = 200
     __HEIGHT: int = 100
 
-    def __init__(self, callback: callable, x: int = __X, y: int = __Y, width: int = __WIDTH, height: int = __HEIGHT):
-        super().__init__(callback, x, y, width, height)
+    def __init__(self, base:QWidget, callback: callable, x: int = __X, y: int = __Y, width: int = __WIDTH, height: int = __HEIGHT, axes:tuple[int] = (0, 0)):
+        super().__init__("",base, callback, x, y, width, height, axes)
+        
+        self.setStyleSheet("""
+            QPushButton {
+                background-color: lightgray;
+                color: black;
+                border: 1px solid black;
+                border-radius: 5px;
+                padding: 5px;
+            }            
+        """)
+        
+        self.set_data("test", datetime.datetime.now(), pathlib.Path("/image/"))
+        
 
     def set_data(self, name: str, dt: datetime.datetime, path: pathlib.Path):
-        pass
+        self.setText(f"""
+        name : {name}
+        dt : {dt}
+        path : {path}         
+        """)
+    
+    
