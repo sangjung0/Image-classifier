d2b6043d57d3dbfaa6d808a4bee355e4b455d242
SangJung <sangjung245@gmail.com>
2024-05-25 04:44:26 +0000

    [FEAT] Frame 클래스 구현
    init -> x, y, width, height 값으로 위젯 위치 및 크기 세팅
    
    set_iamge_layer -> Path를 받고 image_layer 생성하기 위하여 setter 생성
    
    init_ui -> ui 초기 위치 설정
    
    resizeEvent -> Frame 리사이즈 될때마다 자식 위젯들 리사이즈
    
    resize_widget -> 과도한 이미지 랜더링을 피하기 위한 딜레이 랜더링 이벤트 함수

--- program/gui/Frame.py
+++ program/gui/Frame.py
@@ -1,10 +1,81 @@
-class Frame:
+import sys
+from typing import Type
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QResizeEvent
+from PyQt5.QtCore import QTimer
+
+from gui.imageLayer import ImageLayer
+from gui.ImagePanel import ImagePanel
+
+from core import DataController
+
+class Frame(QWidget):
+    """
+    프로그램 기반 위젯 생성
+    """
+    
+    __TITLE:str = "Image Classifier"
+    __RESIZE_INTERVAL:int = 100
 
     def __init__(self, x: int, y: int, width: int, height: int):
-        self.__x = x
-        self.__y = y
-        self.__width = width
-        self.__height = height
+        """
+        x -- int >= 0
+        y -- int >= 0
+        width -- int >= 0 
+        height -- int >= 0
+        """
+        super().__init__()
+        self.__x:int = x
+        self.__y :int= y
+        self.__width:int = width
+        self.__height:int = height
+        self.__image_panel: ImagePanel = ImagePanel(self)
+        self.__image_layer: ImageLayer = None # 생각해보자
+        
+        # 이미지 딜레이 리프레쉬
+        self.__resize_timer = QTimer(self)
+        self.__resize_timer.setSingleShot(True)
+        self.__resize_timer.timeout.connect(self.resize_widget)
+        
+        self.init_ui()
+        
+    def set_image_layer(self, data_controller:DataController, ImageLayer_:Type[ImageLayer] = ImageLayer):
+        """
+        Image Layer 생성
+        
+        data_controller -- DataController
+        ImageLayer_ -- ImageLayer (class. not instance)
+        """
+        self.__image_layer = ImageLayer_(self, self.__image_panel, data_controller)
+        self.__image_layer.raise_()
+        
+    def init_ui(self):
+        self.setWindowTitle(self.__TITLE)
+        self.setGeometry(self.__x, self.__y, self.__width, self.__height)
+        self.setStyleSheet("background-color: black;")
 
-    def _show(self):
-        pass
+        self.__image_panel.setGeometry(0,0,self.__width, self.__height)
+        
+    def resizeEvent(self, event:QResizeEvent):
+        size = event.size()
+        self.__image_layer.setGeometry(0, 0, size.width(), size.height())
+        self.__resize_timer.start(Frame.__RESIZE_INTERVAL)
+        super().resizeEvent(event)
+        
+    def resize_widget(self):
+        """
+        resize timer event function
+        """
+        size = self.size()
+        self.__image_layer.setGeometry(0, 0, size.width(), size.height())
+        self.__image_panel.draw_image(size = size)
+
+# test code
+def main():
+    app = QApplication(sys.argv)
+    main = Frame(100, 100, 500, 500)
+    main.show()
+    sys.exit(app.exec())
+    
+if __name__ == "__main__":
+    pass
