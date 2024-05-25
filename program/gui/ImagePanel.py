b5ec2a826b6297a0b2d973a206b3bc6fc8bed341
SangJung <sangjung245@gmail.com>
2024-05-25 05:54:29 +0000

    [MODIFY] ImagePanel 그리기와 리사이징 분리
    resize_image를 추가하여 이미지 그리기와 리사이징을 분리하여 효율적으로 동작하게 하였으나 의미 없음. QLabel의 한계로 판단됨. 수정 필요

--- program/gui/ImagePanel.py
+++ program/gui/ImagePanel.py
@@ -15,10 +15,12 @@ class ImagePanel(QLabel):
 
     def __init__(self, base:QWidget):
         super().__init__(base)
+        self.__parent = base
         self.__image: np.ndarray = ImagePanel.__DEFAULT_IMAGE
         self.__scale: float = 1
         self.setAlignment(Qt.AlignCenter)
         self.setStyleSheet("background-color: orange;")
+        self.draw_image()
         
     def set_image(self, image: np.ndarray):
         """ 
@@ -33,9 +35,24 @@ class ImagePanel(QLabel):
             self.__image = image.astype(np.uint8)
         self.draw_image()
         
-    def draw_image(self, n:int = 0, size:QSize = None):
+    def draw_image(self):
         """
         이미지 그리기
+        """
+        
+        image_scale, _ = self.__resize_image(self.size())
+        image:np.ndarray =  np.array(Image.fromarray(self.__image).resize(image_scale, Image.LANCZOS))
+        self.setPixmap(QPixmap(QImage(
+            image.data,
+            image.shape[1],
+            image.shape[0],
+            image.shape[1] * 3,
+            QImage.Format.Format_RGB888
+        )))
+        
+    def resize_image(self, scale:int = 0, size:QSize = None):
+        """
+        이미지 리사이즈
         
         n -- 이미지 확대 축소 비율.
             n > 0 -> 이미지 확대
@@ -44,12 +61,19 @@ class ImagePanel(QLabel):
         size -- QSize 조정하고 싶은 크기
         """
         
-        self.__scale = self.__scale + n/100 
-        if 0.98 < self.__scale < 1.02: self.__scale = 1
-        if size is None: 
-            size = self.size()
+        scale = self.__scale + scale/1000 
+        if 0.98 < scale < 1.02: scale = 1
+        if scale > 2: scale = 2
+        elif scale < 0.5: scale = 0.5
         
-        image = self.__resize_image(size)
+        if size is None:
+            if scale == self.__scale: return
+            self.__scale = scale 
+            size = self.__parent.size()
+            
+        image_scale, frame_scale = self.__resize_image(size)
+        self.setGeometry(*frame_scale)
+        image:np.ndarray =  np.array(Image.fromarray(self.__image).resize(image_scale, Image.LANCZOS))
         self.setPixmap(QPixmap(QImage(
             image.data,
             image.shape[1],
@@ -57,8 +81,9 @@ class ImagePanel(QLabel):
             image.shape[1] * 3,
             QImage.Format.Format_RGB888
         )))
+        
 
-    def __resize_image(self, size:QSize):
+    def __resize_image(self, size:QSize) -> tuple[tuple[int], tuple[int]]:
         """
         이미지 크기 조정
         
@@ -77,17 +102,17 @@ class ImagePanel(QLabel):
             
         width = int(width * self.__scale)
         height = int(height * self.__scale)
+        image_scale = (width, height)
         
-        if self.__scale > 1:
-            x = 0
-            y = 0
-            if width > frame_width:
-                x = (frame_width - width) // 2
-            if height > frame_height:
-                y = (frame_height - height) // 2
-            self.setGeometry(x, y, width, height)
-        else:
-            self.setGeometry(0, 0, frame_width, frame_height)
-            
-        return np.array(Image.fromarray(self.__image).resize((width, height), Image.LANCZOS))
+        x = 0
+        y = 0
+        if width > frame_width:
+            x = (frame_width - width) // 2
+        if height > frame_height:
+            y = (frame_height - height) // 2
+        width = width if frame_width < width else frame_width
+        height = height if frame_height < height else frame_height
+        frame_scale = (x, y, width, height)
+
+        return image_scale, frame_scale
         
