ce4801f6025234e4b745c1a629f8c30c40b4e342
SangJung <sangjung245@gmail.com>
2024-05-25 04:53:51 +0000

    [FEAT] callback 함수 실행 방법 변경 및 버튼 이름 변수 추가
    __exit -> remove
    closeEvent -> __exit 이벤트를 대체함. 종료 이벤트가 승인 이벤트로 인한 종료가 아닐시 cacel callback 함수를 실행하게 함

--- program/gui/pathModal/PathModal.py
+++ program/gui/pathModal/PathModal.py
@@ -1,6 +1,7 @@
 import pathlib
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QPushButton, QFileDialog, QHBoxLayout
 from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QCloseEvent
 
 from gui.Utils import message_box
 
@@ -11,15 +12,23 @@ class PathModal(QWidget):
     """
     __WIDTH:int = 600
     __HEIGHT:int = 50
-    def __init__(self, center:QPoint, success_callback: callable, cacel_callback:callable, success_name: str):
+    def __init__(self, center:QPoint, confirm_callback: callable, cancel_callback:callable, confirm_name: str, cancel_name: str):
+        """
+        center -- QPoint 모달 center 위치
+        confirm_callback -- callable 성공시 실행 함수
+        cancel_callback -- callable 실패시 실행 함수
+        confirm_name -- 성공 버튼 이름
+        cancel_name -- 취소 버튼 이름
+        """
         super().__init__()
         self.__path_input:QLineEdit = QLineEdit(self)
-        self.__success_callback:callable = success_callback
-        self.__cacel_callback: callable = cacel_callback
+        self.__confirm_callback:callable = confirm_callback
+        self.__cancel_callback: callable = cancel_callback
+        self.__status:bool = False
         
-        self.__init_ui(success_name, center)
+        self.__init_ui(confirm_name, cancel_name , center)
     
-    def __init_ui(self, success_name:str, center:QPoint):
+    def __init_ui(self, confirm_name:str, cancel_name:str, center:QPoint):
         """
         Ui 위치 및 이벤트 지정
         """
@@ -27,7 +36,6 @@ class PathModal(QWidget):
         h_layout = QHBoxLayout()
         
         self.__path_input.setPlaceholderText("Enter path or click 'Browse' to select")
-        self.__path_input.textChanged.connect(self.__text_line_event)
         h_layout.addWidget(self.__path_input, 8)
         
         browse_button = QPushButton("Browse", self)
@@ -38,12 +46,12 @@ class PathModal(QWidget):
         
         h_layout = QHBoxLayout()
         
-        success_button = QPushButton(success_name, self)
+        success_button = QPushButton(confirm_name, self)
         success_button.clicked.connect(self.__set_event)
         h_layout.addWidget(success_button, 5)
         
-        cancel_button = QPushButton("Exit", self)
-        cancel_button.clicked.connect(self.__exit)
+        cancel_button = QPushButton(cancel_name, self)
+        cancel_button.clicked.connect(self.close)
         h_layout.addWidget(cancel_button,5)
         
         v_layout.addLayout(h_layout)
@@ -51,18 +59,21 @@ class PathModal(QWidget):
         self.setLayout(v_layout)
         self.setWindowTitle("Select Path")
         
+        print(center.x(), center.y())
         self.setGeometry(center.x() - PathModal.__WIDTH//2, center.y() - PathModal.__HEIGHT//2, PathModal.__WIDTH, PathModal.__HEIGHT)
         
-    def __exit(self):
-        self.close()
-        self.__cacel_callback()
+    def closeEvent(self, event:QCloseEvent):
+        if not self.__status: 
+            self.__cancel_callback()
+        super().closeEvent(event)
+        
 
     def __button_event(self):
         """
         Browse 버튼 이벤트
         """
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
+        #options |= QFileDialog.DontUseNativeDialog
         result = QFileDialog.getExistingDirectory(self, "Select Folder", options=options)
         if result:
             self.__path_input.setText(result)
@@ -73,8 +84,10 @@ class PathModal(QWidget):
         """
         path = pathlib.Path(self.__path_input.text())
         if path.exists() and path.is_dir():
-            self.__success_callback(path)
+            self.__status = True
+            self.__confirm_callback(path)
             self.close()
         else:
+            self.__status = False
             message_box(self, "Invalid path", "The specified path does not exist or is not a directory. Please try again.")
         
