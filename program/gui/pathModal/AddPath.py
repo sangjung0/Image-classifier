0c695525e0b8654ddf9463ac03932e1e160c8b36
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-01 11:31:24 +0000

    [MODIFY] PathModal 매개변수 변경에 따라 생성자 수정

--- program/gui/pathModal/AddPath.py
+++ program/gui/pathModal/AddPath.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QPoint
+from PyQt5.QtWidgets import QWidget
 
 from gui.pathModal.PathModal import PathModal
 
@@ -7,10 +7,9 @@ class AddPath(PathModal):
     __CONFIRM_NAME: str = "Add"
     __CANCEL_NAME: str = "Cancel"
 
-    def __init__(self, center:QPoint, success_callback: callable, cancel_callback: callable):
+    def __init__(self, base:QWidget, callback: callable):
         """
-        center -- QPoint 모달 center 위치
-        confirm_callback -- callable 성공시 실행 함수
-        cancel_callback -- callable 실패시 실행 함수
+        base -- QWidget 모달 종속 위젯
+        callback -- callable 성공시 실행 함수
         """
-        super().__init__(center, success_callback, cancel_callback, AddPath.__CONFIRM_NAME, AddPath.__CANCEL_NAME)
+        super().__init__(base, callback, AddPath.__CONFIRM_NAME, AddPath.__CANCEL_NAME)
--- program/gui/pathModal/Init.py
+++ program/gui/pathModal/Init.py
@@ -1,4 +1,3 @@
-from PyQt5.QtCore import QPoint
 
 from gui.pathModal.PathModal import PathModal
 
@@ -8,10 +7,9 @@ class Init(PathModal):
     __CANCEL_NAME: str = "Exit"
     
 
-    def __init__(self, center:QPoint, success_callback: callable, cancel_callback: callable):
+    def __init__(self, callback: callable):
         """
-        center -- QPoint 모달 center 위치
-        confirm_callback -- callable 성공시 실행 함수
-        cancel_callback -- callable 실패시 실행 함수
+        base -- QWidget 모달 종속 위젯
+        callback -- callable 성공시 실행 함수
         """
-        super().__init__(center, success_callback, cancel_callback, Init.__CONFIRM_NAME, Init.__CANCEL_NAME)
+        super().__init__(None, callback, Init.__CONFIRM_NAME, Init.__CANCEL_NAME)
