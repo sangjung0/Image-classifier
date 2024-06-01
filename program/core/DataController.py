4e543fb6ea9481dd50d6eb66cf59497bb6fb38df
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-01 12:18:56 +0000

    [FEAT] 캐시 기능 구현

--- program/core/DataController.py
+++ program/core/DataController.py
@@ -6,27 +6,37 @@ from core.dto import Data, Image
 
 
 class DataController:
+    
+    __CACHE_SIZE = 10
+    
     def __init__(self, image_path: list[pathlib.Path], sub_path: list[pathlib.Path]):
         self.__image_path: list[pathlib.Path] = image_path
         self.__cache: dict[pathlib.Path: np.ndarray]
         self.__images: Data
         self.__searcher: Searcher
-        self._sub_path: list[pathlib.Path] # 이거 뭐지
+        self._sub_path: list[pathlib.Path] # 뭐긴 뭐야 폴더 셋이지 ㅋ
         self._cnt_image_index:int = len(self.__image_path)-1
 
-    def get_cnt_image(self) -> Image:
+    def get_cnt_image(self) -> tuple[Image, np.ndarray]:
         if(self._cnt_image_index < 0): return None
-        return self.__images.get_image(self.__image_path[self._cnt_image_index])        
+        path = self.__image_path[self._cnt_image_index]
+        image = self.__images.get_image(path)
+        if(path in self.__cache):
+            ary = self.__cache[path]
+        else:
+            ary = image.get_image()
+            self.__cache[path] = ary                
+        return image, ary
 
-    def get_next_image(self) -> Image:
+    def get_next_image(self) -> tuple[Image, np.ndarray]:
         if(self._cnt_image_index < 0): return None
-        self._cnt_image_index = (self._cnt_image_index + 1) % len(self.__image_path)
-        return self.__images.get_image(self.__image_path[self._cnt_image_index])
+        self._cnt_image_index = self.__image_index(1)
+        return self.get_cnt_image()
 
-    def get_prev_image(self) -> Image:
+    def get_prev_image(self) -> tuple[Image, np.ndarray]:
         if(self._cnt_image_index < 0): return None
-        self._cnt_image_index = (self._cnt_image_index - 1 + len(self.__image_path)) % len(self.__image_path)
-        return self.__images.get_image(self.__image_path[self._cnt_image_index])
+        self._cnt_image_index = self.__image_index(-1)
+        return self.get_cnt_image()
 
     def search_image(self) -> 'DataController': pass
 
@@ -35,3 +45,10 @@ class DataController:
     def stop_search(self): pass
 
     def move(self, n: int): pass
+    
+    def __image_index(self, value:int) -> None:
+        return (self._cnt_image_index + value) % len(self.__image_path)
+    
+    def __cache_clear(self) -> None:
+        while True:
+            pass
