afc2e133662e5a2bcc66f4833fd9ddaba7ebbd9a
SangJung <sangjung245@gmail.com>
2024-06-04 15:45:49 +0900

    [FEAT] move 함수, close 함수 그리고 cache manager 함수 추가
    move -> 이미지 파일 이동 함수. 이미지를 삽입하고 기존 주소 위치에 새로운 주소를 덮어 씀
    
    __chache_manager -> 이미지 cache 메모리 관리자 함수 추가 현재 이미지 기반 다음 탐색 이미지 추측하여 미리 캐시에 올려서 탐지 속도를 올림
    
    close -> __chache_manager 쓰레드 종료 함수

--- program/core/DataController.py
+++ program/core/DataController.py
@@ -1,5 +1,7 @@
 import pathlib
 import numpy as np
+import time
+import threading
 
 from core.Searcher import Searcher
 from core.dto import Data, Image
@@ -7,15 +9,25 @@ from core.dto import Data, Image
 
 class DataController:
     
-    __CACHE_SIZE = 10
+    __CACHE_SIZE:int = 11
     
-    def __init__(self, image_path: list[pathlib.Path], sub_path: list[pathlib.Path]):
+    def __init__(self, image_path: list[pathlib.Path], sub_path: dict[int: pathlib.Path], data:Data = Data()):
         self.__image_path: list[pathlib.Path] = image_path
-        self.__cache: dict[pathlib.Path: np.ndarray]
-        self.__images: Data
+        self.__images: Data = data
+        self.__sub_path: dict[int:pathlib.Path] = sub_path
+        
         self.__searcher: Searcher
-        self._sub_path: list[pathlib.Path] # 뭐긴 뭐야 폴더 셋이지 ㅋ
-        self._cnt_image_index:int = len(self.__image_path)-1
+        
+        self.__cache: dict[pathlib.Path: np.ndarray] = dict()
+        self._cnt_image_index:int = 0
+        
+        self.__lock:threading.Lock = threading.Lock()
+        self.__flag:bool = False
+        
+        # cache manager
+        self.__thread:threading.Thread = threading.Thread(target = self.__cache_manager)
+        self.__thread.start()
+
 
     def get_cnt_image(self) -> tuple[Image, np.ndarray]:
         if(self._cnt_image_index < 0): return None
@@ -44,11 +56,41 @@ class DataController:
 
     def stop_search(self): pass
 
-    def move(self, n: int): pass
+    def move(self, n: int): 
+        src = self.__image_path[self._cnt_image_index]
+        dest:pathlib.Path = self.__sub_path[n] / src.name
+        src.rename(dest)
+        self.__images.delete(src)
+        index = self.__image_path.index(src)
+        self.__image_path[index] = dest
+        
     
     def __image_index(self, value:int) -> None:
         return (self._cnt_image_index + value) % len(self.__image_path)
-    
-    def __cache_clear(self) -> None:
+        
+    def __cache_manager(self) -> None:
+        start = DataController.__CACHE_SIZE//2
         while True:
-            pass
+            time.sleep(0.1)
+            if self.__flag: break
+            cnt = self._cnt_image_index
+            length = len(self.__image_path)
+            a = set(self.__image_path[(cnt+i)%length] for i in range(-start, start, 1))
+            b = set(self.__cache.keys())
+            a, b = b-a, a-b
+            i = 0
+            while i < len(a):
+                if cnt != self._cnt_image_index:
+                    break
+                with self.__lock:
+                    del self.__cache[b[i]]
+                image = self.__images.get_image(a[i])
+                with self.__lock:
+                    self.__cache[a[i]] = image
+                    
+    def close(self) -> None:
+        self.__flag = True
+        if self.__thread != None:
+            self.__thread.join()
+                
+            
