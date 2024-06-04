1c294bd06e92abed21fe137f4baf7f681513d97f
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-04 13:03:12 +0000

    [FEAT] 폴더 추가 구현 및 폴더 꽉 찾는지 확인하는 메소드 추가

--- program/core/MainDataController.py
+++ program/core/MainDataController.py
@@ -2,20 +2,36 @@ import pathlib
 from threading import Lock
 
 
-from program.core.Storage import Storage
-from program.core.AutoSave import AutoSave
-from program.core.DataController import DataController
-from program.core.scheduler import Scheduler
+from core.Storage import Storage
+from core.AutoSave import AutoSave
+from core.DataController import DataController
+from core.scheduler import Scheduler
+from core.dto import Data
+
+from utils import PathData
 
 class MainDataController(DataController):
-    def __init__(self, path: pathlib.Path):
+    
+    __MAX_PATH_COUNT:int = 4
+    
+    def __init__(self, path: pathlib.Path, sub_path:list[pathlib.Path] = []):
+        paths = list(PathData(path))
+        data = Data()
+        data.search(paths)
+        super().__init__(paths, sub_path, data)
+        self.__sub_path:list[pathlib.Path] = sub_path
+        
         self.__storage:Storage
         self.__auto_save:AutoSave
         self.__scheduler:Scheduler
         self.__lock:Lock
 
-        super().__init__()
-
     def organization(self) : pass
 
-    def add_path(self, add_path:pathlib.Path): pass
+    def add_path(self, add_path:pathlib.Path) -> None:
+        if len(self.__sub_path) >= MainDataController.__MAX_PATH_COUNT: raise Exception()
+        self.__sub_path.append(add_path)
+        
+    def sub_path_is_full(self) -> bool:
+        if len(self.__sub_path) >= MainDataController.__MAX_PATH_COUNT: return True
+        return False 
