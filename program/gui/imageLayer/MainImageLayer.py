1ed951a43fc6c05df1cd16f34dc2979196dda6dd
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-04 13:03:36 +0000

    [FEAT] 폴더 추가 이벤트 구현

--- program/gui/imageLayer/MainImageLayer.py
+++ program/gui/imageLayer/MainImageLayer.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import  QWidget
-from PyQt5.QtGui import QResizeEvent, QCloseEvent
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QResizeEvent
+import pathlib
 
 from gui import ImagePanel
 from gui.imageLayer.interactiveObject import *
@@ -9,10 +9,12 @@ from gui.imageLayer.ImageLayer import ImageLayer
 
 from core import MainDataController
 
+from gui.Utils import message_box
+
 class MainImageLayer(ImageLayer):
     def __init__(self, base:QWidget, image_panel: ImagePanel, data_controller:MainDataController):
         super().__init__(base, image_panel, data_controller)
-        self.__parent = base
+        self.__data_controller:MainDataController = data_controller
         self.__new_folder: NewFolder = NewFolder(self, self.__new_folder_event, 0.5, 0.01, 100, 50, (-110, 0))
         self.__auto_save: AutoSave = AutoSave(self, self.__auto_organization, 0.5, 0.01, 100, 50, (10, 0))
         self.__add_path: AddPath = AddPath(self, self.__add_folder_event)
@@ -26,7 +28,9 @@ class MainImageLayer(ImageLayer):
     def __new_folder_event(self):
         self.__add_path.show(self.mapToGlobal(self.geometry().center()))
         
-    def __add_folder_event(self, path): pass
+    def __add_folder_event(self, path:pathlib.Path):
+        if self.__data_controller.sub_path_is_full():
+            message_box(self, "add path error", "sub path is full")
+        else: self.__data_controller.add_path(path)
         
-    
     def __auto_organization(self): pass
