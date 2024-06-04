227fffd8ea5f8a3fa945a0fa13872dcd9a7ca4cb
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-04 04:27:00 +0000

    [STYLE] 주석 추가

--- program/core/dto/Face.py
+++ program/core/dto/Face.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 class Face:
+    """
+    얼굴의 이름 번호와 이미지 데이터를 가지고 있음
+    """
     def __init__(self, name: int, image: np.ndarray):
         self.__image: np.ndarray = image 
         self.__name: int = name
