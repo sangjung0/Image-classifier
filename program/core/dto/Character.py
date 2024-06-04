7ae7599c38a3e04cb1704a1ed2269a127a1d78d5
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-04 04:27:22 +0000

    [FEAT] Character 클래스 구현

--- program/core/dto/Character.py
+++ program/core/dto/Character.py
@@ -1,10 +1,13 @@
 class Character:
+    """
+    사진에서의 얼굴 위치와 이름을 가지고 있음
+    """
     def __init__(self, name: int, location: tuple[int]):
-        self.__name: int
-        self.__location: tuple[int]
+        self.__name: int = name
+        self.__location: tuple[int] = location
 
     def get_name(self) -> int:
-        pass
+        return self.__name
 
     def get_location(self) -> tuple[int]:
-        pass
+        return self.__location
