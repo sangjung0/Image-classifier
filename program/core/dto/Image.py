c97614cff431e9a5af0cd92122b06dc9e9fbf2ce
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-04 04:26:46 +0000

    [FEAT] 이미지 클래스 구현

--- program/core/dto/Image.py
+++ program/core/dto/Image.py
@@ -1,49 +1,82 @@
 import pathlib
 import datetime
+import PIL
+import PIL.Image
+from PIL.ExifTags import TAGS
 import numpy as np
 
-from program.core.dto.Character import Character
+from core.dto.Character import Character
 
 
 class Image:
+    """
+    이미지 정보를 가지고 있음
+    이미지 경로, 이름, 날짜, 시간, 등장인물, 해시 값 등의 메타데이터와 이미지 자체 데이터
+    """
     def __init__(self, path: pathlib.Path):
-        self.__path: pathlib.Path
-        self.__name: str
-        self.__date: datetime.datetime
-        self.__time: datetime.time
-        self.__characters: tuple[Character]
-        self.__hash: bin
-        self.__is_detected: bool
+        self.__path: pathlib.Path = path
+        self.__name: str = path.name
+
+        self.__img: PIL.Image.Image = None
+        self.__date: datetime.datetime = None
+        self.__time: datetime.time = None
+        self.__characters: list[Character] = None
+        self.__hash: bin = None
+        self.__is_detected: bool = False
+        
+        self.__set_data()
+        
+    def __set_data(self):
+        """
+        이미지 메타데이터 읽은 후 매개변수 초기화
+        """
+        self.__img = PIL.Image.open(self.__path)
+        exif_data = self.__img.getexif()
+        
+        if exif_data is not None:
+            exif = {
+                TAGS.get(tag): value
+                for tag, value in exif_data.items()
+                if tag in TAGS
+            }
+            
+            date_time_str = exif.get('DateTime', 'N/A')
+            
+            if date_time_str != 'N/A':
+                date_time_obj = datetime.datetime.strptime(date_time_str, '%Y:%m:%d %H:%M:%S')
+                self.__date = date_time_obj.date()
+                self.__time = date_time_obj.time()
+        
 
     def get_path(self) -> pathlib.Path:
-        pass
+        return self.__path
 
     def get_name(self) -> str:
-        pass
+        return self.__name
 
-    def get_data(self) -> datetime.datetime:
-        pass
+    def get_date(self) -> datetime.datetime:
+        return self.__date
 
     def get_time(self) -> datetime.time:
-        pass
+        return self.__time
 
     def get_characters(self) -> tuple[Character]:
-        pass
+        return self.__characters
 
     def get_hash(self) -> bin:
-        pass
+        return self.__hash
 
     def get_image(self) -> np.ndarray:
-        pass
+        return np.array(self.__img)
 
     def is_detected(self) -> bool:
-        pass
+        return self.__is_detected
 
-    def set_characters(self, characters: tuple[Character]):
-        pass
+    def set_characters(self, characters: list[Character]):
+        self.__characters.append(*characters)
 
     def set_is_detected(self, b: bool):
-        pass
+        self.__is_detected = b
 
     def set_hash(self, _hash: bin):
-        pass
+        self.__hash = _hash
