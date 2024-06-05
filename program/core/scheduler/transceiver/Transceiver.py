c38cf70225403aa5c10ddd0d3a6d84af1e6bad88
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 02:23:33 +0000

    [FEAT] 송수신기 인터페이스 구현

--- program/core/scheduler/transceiver/Transceiver.py
+++ program/core/scheduler/transceiver/Transceiver.py
@@ -1,17 +1,25 @@
 from threading import Lock
+from collections import deque
 
 from program.core.scheduler.dto import Packet
 
 
 class Transceiver:
     def __init__(self):
-        self.__lock: Lock
-        self.__data: list[object]
+        self.__lock: Lock = Lock()
+        self.__data: deque[Packet] = deque()
 
-    def __len__(self) -> int: pass
+    def __len__(self) -> int:
+        return len(self.__data)
 
-    def append(self, value: Packet): pass
+    def append(self, value: Packet):
+        with self.__lock:
+            self.__data.append(value)
 
-    def get(self) -> object: pass
+    def get(self) -> object:
+        if self.is_empty(): return None
+        with self.__lock:
+            return self.__data.popleft()
 
-    def is_empty(self) -> bool: pass
+    def is_empty(self) -> bool:
+        return len(self.__data) == 0
