40eed7fec0c2694af3c2b4fc3def7db48b9d6f4f
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 10:33:55 +0000

    [FEAT] set_path  메소드 추가

--- program/core/scheduler/dto/PacketData.py
+++ program/core/scheduler/dto/PacketData.py
@@ -1,11 +1,11 @@
 import pathlib
 import numpy as np
 
-from program.core.dto import Face, Character
+from core.dto import Face, Character
 
 
 class PacketData:
-    def __init__(self, path: pathlib.Path):
+    def __init__(self, path: pathlib.Path) -> None:
         self.__path: pathlib.Path = path
         self.__image: np.ndarray = None
         self.__hash: bin = None
@@ -26,15 +26,18 @@ class PacketData:
 
     def get_chracters(self) -> list[Character]:
         return self.__chracters
+    
+    def set_path(self, path:pathlib.Path) -> None:
+        self.__path = path
 
-    def set_face(self, face: Face):
+    def set_face(self, face: Face) -> None:
         self.__faces.append(face)
 
-    def set_character(self, character: Character):
+    def set_character(self, character: Character) -> None:
         self.__chracters.append(character)
 
-    def set_image(self, image: np.ndarray | bytes):
+    def set_image(self, image: np.ndarray | bytes) -> None:
         self.__image = image
 
-    def set_hash(self, hash_: bin):
+    def set_hash(self, hash_: bin) -> None:
         self.__hash = hash_
