e68ac4337124d0d9edc4005e4790fbb0ab720cde
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 10:34:18 +0000

    [MODIFY] buf_size 변수 제거

--- program/core/scheduler/processor/Processor.py
+++ program/core/scheduler/processor/Processor.py
@@ -4,16 +4,15 @@ from threading import Thread
 import os
 import time
 
-from program.core.scheduler.transceiver import Receiver, Sender
-from program.core.scheduler.dto import Packet
+from core.scheduler.transceiver import Receiver, Sender
+from core.scheduler.dto import Packet
 from core.scheduler.Constant import PROCESSOR_STOP, PROCESSOR_PAUSE
 
 from utils import Loger, Timer
 
 class Processor:
-    def __init__(self, name:str, buf_size: int, loger_is_print:bool = False):
+    def __init__(self, name:str, loger_is_print:bool = False):
         self.__name:str = name
-        self.__buf_size: int = buf_size
         self.__loger_is_print:bool = loger_is_print
 
     def __call__(self, order: int, termination_signal: SynchronizedBase, flag: SynchronizedBase, input_q:Queue, output_q:Queue):
@@ -27,8 +26,8 @@ class Processor:
             sender_thread = Thread(target=sender, args=(order, termination_signal, flag, output_q))
             sender_thread.start()
             
-            receiver = Receiver(self.__name + "Sender", self.__buf_size, self.__loger_is_print)
-            receiver_thread = Thread(target=receiver, args=(order, termination_signal, flag, input_q))
+            receiver = Receiver(self.__name + "Sender", self.__loger_is_print)
+            receiver_thread = Thread(target=receiver, args=(order+1, termination_signal, flag, input_q))
             receiver_thread.start()
             
             while True:
@@ -47,11 +46,12 @@ class Processor:
                         result = timer.measure(lambda : self.processing(data))
                         loger("데이터 연산", option=timer)
                         sender.append(result)
-            
         except Exception as e:
             flag.value = PROCESSOR_STOP
             loger(os.getpid(), "오류", e)
-            
+        receiver_thread.join()
+        sender_thread.join()
+        loger(os.getpid(), "종료", option="terminate")
         
     def processing(self, value:Packet):
         raise Exception("구현 안됨")
