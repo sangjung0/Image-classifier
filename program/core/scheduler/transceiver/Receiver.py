1419c59782624554ecb404599f9c183e23fa7e1b
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 07:49:48 +0000

    [FEAT] buf size 제거 및 is_full 함수 제거.
    buf_size, is_full 변수 제거 -> queue의 버프 사이즈 조정으로 필요 없어짐

--- program/core/scheduler/transceiver/Receiver.py
+++ program/core/scheduler/transceiver/Receiver.py
@@ -10,10 +10,9 @@ from utils import Loger, Timer
 
 
 class Receiver(Transceiver):
-    def __init__(self, name:str, buf_size:int, loger_is_print:bool=False) -> None:
+    def __init__(self, name:str, loger_is_print:bool=False) -> None:
         super().__init__()
         self.__name:str = name
-        self.__buf_size:int = buf_size
         self.__loger_is_print:bool = loger_is_print
 
     def __call__(self, order: int, termination_signal: SynchronizedBase, flag: SynchronizedBase, queue: Queue):
@@ -30,24 +29,18 @@ class Receiver(Transceiver):
                 elif flag.value == PROCESSOR_PAUSE:
                     time.sleep(0.1)
                 else:
-                    if queue.empty():
-                        if termination_signal.value >= order:
-                            break
-                        time.sleep(0.01)
-                    elif self.is_full():
-                        time.sleep(0.01)
-                    else:
+                    try:
                         ret, data = timer.measure(lambda : converter.receive())
                         loger("데이터 수신 후 압축 해제", option=timer)
                         if ret: 
                             self.append(data)
+                    except Exception as _:
+                        if termination_signal.value >= order:
+                            break
         except Exception as e:
             loger("쓰레드 오류",e, option="error") # loger
             flag.value = PROCESSOR_STOP
         loger(f"데이서 수신 후 압축 해제 평균 속도 {timer.average}", option='result') # loger
         loger("쓰레드 종료", option='terminate') # loger
-        return
 
-    def is_full(self) -> bool:
-        return len(self) == self.__buf_size
 
