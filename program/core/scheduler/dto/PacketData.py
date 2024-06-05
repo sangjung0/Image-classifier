a47da7fc0d0c42bc9444b474910db8b4c3b20c48
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 02:24:03 +0000

    [FEAT] PacketData 구현

--- program/core/scheduler/dto/PacketData.py
+++ program/core/scheduler/dto/PacketData.py
@@ -6,27 +6,35 @@ from program.core.dto import Face, Character
 
 class PacketData:
     def __init__(self, path: pathlib.Path):
-        self.__path: pathlib.Path
-        self.__image: np.ndarray
-        self.__hash: bin
-        self.__faces: list[Face]
-        self.__chracters: list[Character]
+        self.__path: pathlib.Path = path
+        self.__image: np.ndarray = None
+        self.__hash: bin = None
+        self.__faces: list[Face] = []
+        self.__chracters: list[Character] = []
 
-    def get_image(self) -> np.ndarray:
-        pass
+    def get_image(self) -> np.ndarray | bytes:
+        return self.__image
 
-    def get_path(self) -> pathlib.Path: pass
+    def get_path(self) -> pathlib.Path:
+        return self.__path
 
-    def get_hash(self) -> bin: pass
+    def get_hash(self) -> bin:
+        return self.__hash
 
-    def get_faces(self) -> list[Face]: pass
+    def get_faces(self) -> list[Face]:
+        return self.__faces
 
-    def get_chracters(self) -> list[Character]: pass
+    def get_chracters(self) -> list[Character]:
+        return self.__chracters
 
-    def set_face(self, face: Face): pass
+    def set_face(self, face: Face):
+        self.__faces.append(face)
 
-    def set_character(self, character: Character): pass
+    def set_character(self, character: Character):
+        self.__chracters.append(character)
 
-    def set_image(self, image: np.ndarray): pass
+    def set_image(self, image: np.ndarray | bytes):
+        self.__image = image
 
-    def set_hash(self, hash_: bin): pass
+    def set_hash(self, hash_: bin):
+        self.__hash = hash_
