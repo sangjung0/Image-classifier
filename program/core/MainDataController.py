c4a7acb445eb40b5583104ec4ca6e8b51a642d0c
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-05 10:28:32 +0000

    [FEAT] 스케줄러 추가 및 현재 이미지 함수 오버라이딩과 CLOSE 함수 추가
    현재 이미지 함수를 오버라이딩 하여 스케줄러에 현재 이미지를 우선적으로 탐지 하라는 명령을 전달함.
    
    close 함수를 오버라이딩하여 쓰레드 안정적인 종료

--- program/core/MainDataController.py
+++ program/core/MainDataController.py
@@ -1,20 +1,23 @@
 import pathlib
 from threading import Lock
+import numpy as np
 
 
 from core.Storage import Storage
 from core.AutoSave import AutoSave
 from core.DataController import DataController
 from core.scheduler import Scheduler
-from core.dto import Data
+from core.dto import Data, Image
 
 from utils import PathData
 
 class MainDataController(DataController):
     
     __MAX_PATH_COUNT:int = 4
+    __BUF_SIZE:int = 5
+    __PACKET_SIZE:int = 32
     
-    def __init__(self, path: pathlib.Path, sub_path:list[pathlib.Path] = []):
+    def __init__(self, path: pathlib.Path, sub_path:list[pathlib.Path] = []) -> None:
         paths = list(PathData(path))
         data = Data()
         data.search(paths)
@@ -23,8 +26,15 @@ class MainDataController(DataController):
         
         self.__storage:Storage
         self.__auto_save:AutoSave
-        self.__scheduler:Scheduler
+        self.__scheduler:Scheduler = Scheduler(data, paths)
         self.__lock:Lock
+        
+        self.__scheduler.run(MainDataController.__BUF_SIZE, MainDataController.__PACKET_SIZE)
+        
+    def get_cnt_image(self) -> tuple[Image, np.ndarray]:
+        image, ary = super().get_cnt_image()
+        self.__scheduler.add(image.get_path())
+        return image, ary
 
     def organization(self) : pass
 
@@ -35,3 +45,7 @@ class MainDataController(DataController):
     def sub_path_is_full(self) -> bool:
         if len(self.__sub_path) >= MainDataController.__MAX_PATH_COUNT: return True
         return False 
+    
+    def close(self) -> None:
+        self.__scheduler.close()
+        super().close()
