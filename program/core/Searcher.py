6b99a70fda8dc5d261fac8920401e10726526fb2
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-06 12:35:40 +0000

    [FEAT] searcher 구현

--- program/core/Searcher.py
+++ program/core/Searcher.py
@@ -1,12 +1,33 @@
 import pathlib
 
 from core.dto import Data, Image
+from core.Histogram import Histogram
 
 
 class Searcher:
-    def __init__(self, images: Data):
-        self.__images: Data
+    def __init__(self, images: Data, paths: list[pathlib.Path]):
+        self.__images: Data = images
+        self.__paths: list[pathlib.Path] = paths
 
-    def search_image(self, source: Image) -> list[pathlib.Path]: pass
+    def search_image(self, name: int) -> list[pathlib.Path]:
+        result = []
+        for p in self.__paths:
+            image = self.__images.get_image(p)
+            for c in image.characters.values():
+                if c.get_name() == name:
+                    result.append(p)
+                    break
+        return result
+        
 
-    def search_face(self, source: Image) -> list[pathlib.Path]: pass
+    def search_face(self, source: Image) -> list[pathlib.Path]:
+        result = []
+        histogram = source.get_hash()
+        for p in self.__paths:
+            if p == source.get_path(): result.append(p)
+            image = self.__images.get_image(p)
+            if image.is_detected() and Histogram.compare_histograms(histogram, image.get_hash()):
+                result.append(p)
+        return result
+            
+        
