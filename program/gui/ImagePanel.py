6ddb5376d9b3b696385f2b32c7b7b02a68e02fa6
SangJung <124423175+sangjung0@users.noreply.github.com>
2024-06-06 11:42:28 +0000

    [FEAT] 바운더리 박스 추가

--- program/gui/ImagePanel.py
+++ program/gui/ImagePanel.py
@@ -1,8 +1,10 @@
 import numpy as np
 from PIL import Image
-from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QGraphicsRectItem
+from PyQt5.QtGui import QPixmap, QImage, QPen, QColor
 from PyQt5.QtCore import Qt, QPoint, QRectF
+
+from core.dto import Character
 
 class ImagePanel(QGraphicsView):
     """
@@ -19,7 +21,11 @@ class ImagePanel(QGraphicsView):
         """
         super().__init__(base)
         self.__image: np.ndarray = ImagePanel.__DEFAULT_IMAGE
+        self.__faces: dict[int:Character] = {}
+        self.__face_rect: list[QGraphicsRectItem] = []
+        self.__cursor_is_in:bool = True
         self.__scale: float = 1
+        self.__image_scale: float = 1
         self.setStyleSheet("background-color: black;")
         
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -32,24 +38,53 @@ class ImagePanel(QGraphicsView):
         
         self.draw_image()
         
-    def set_image(self, image: np.ndarray) -> None:
+    def set_image(self, face:dict[int:Character], image: np.ndarray) -> None:
         """ 
         이미지는 RGB 순서로 간주함
         
         image -- np.ndarray 이미지
         """
         
+        self.remove_faces()
         if not isinstance(image, np.ndarray):
             self.__image = ImagePanel.__DEFAULT_IMAGE
+            self.__faces = {}
         else:
-            self.__image = image.astype(np.uint8)
+            self.__image = image
+            self.__faces = face
         self.draw_image()
+        self.set_face()
+        self.resize_event()
+        
+    def set_face(self) -> None:
+        pen = QPen(QColor(255, 0, 0), 2, Qt.PenStyle.SolidLine)
+        for i in range(len(self.__faces)):
+            rect = None
+            if i >= len(self.__face_rect):
+                rect = QGraphicsRectItem()
+                rect.setPen(pen)
+                self.__face_rect.append(rect)
+            else:
+                rect = self.__face_rect[i]
+            self.__scene.addItem(rect)
+        
+    def draw_faces(self, x:float, y:float) -> None:
+        for i, face in enumerate(self.__faces.values()):
+            if i >= len(self.__face_rect): self.set_face()
+            rect = self.__face_rect[i]
+            fx, fy, width, height = face.get_location()
+            rect.setRect(0, 0, width*self.__image_scale, height*self.__image_scale )
+            rect.setPos(fx*self.__image_scale+x, fy*self.__image_scale+y)
+    
+    def remove_faces(self) -> None:
+        items = list(set(self.__face_rect) & set(self.__scene.items()))
+        for d in items:
+            self.__scene.removeItem(d)
         
     def draw_image(self) -> None:
         """
         이미지 그리기
         """
-
         self.__image_item.setPos(0, 0)
         self.__image_item.setPixmap(QPixmap.fromImage(QImage(
             self.__image.data,
@@ -57,8 +92,7 @@ class ImagePanel(QGraphicsView):
             self.__image.shape[0],
             self.__image.shape[1] * 3,
             QImage.Format.Format_RGB888
-        )))        
-        self.resize_event()
+        )))
         
     def resize_image(self, scale_factor: int, pos: QPoint) -> None:
         """
@@ -79,9 +113,8 @@ class ImagePanel(QGraphicsView):
             return
         
         scale = self.__scale * scale_factor
-        if scale < 1.05:
-            self.resize_event()
-            return
+        if scale < 1:
+            scale_factor = 1
         elif scale > 5:
             scale_factor = 1
         else:
@@ -90,11 +123,14 @@ class ImagePanel(QGraphicsView):
         old_pos = self.mapToScene(pos)
         self.scale(scale_factor, scale_factor)
         new_pos = self.mapToScene(pos)
-        if self.__scale > 1:
-            self.__image_item.setPos(self.__image_item.pos() + new_pos - old_pos)
-    
+        pos = self.__image_item.pos() + new_pos - old_pos
+        self.__image_item.setPos(pos)
+        
+        self.draw_faces(pos.x(), pos.y())
+            
     def resize_event(self) -> None:
 
+        self.resetTransform()
         self.__scale = 1    
         v_width = self.width()
         v_height = self.height()
@@ -117,9 +153,12 @@ class ImagePanel(QGraphicsView):
                 y = (v_height - height)//2
                 x = (v_width - width)//2
         
+        self.__image_scale = scale
         self.__image_item.setScale(scale)
         self.__image_item.setPos(x, y)
         self.__scene.setSceneRect(QRectF(0, 0, v_width, v_height))
+
+        self.draw_faces(x, y)
         
     def image_move(self, delta:QPoint) -> None:
         """
@@ -140,3 +179,12 @@ class ImagePanel(QGraphicsView):
         elif new_pos.y() < -height:
             new_pos.setY(-height)
         self.__image_item.setPos(new_pos)
+
+        self.draw_faces(new_pos.x(), new_pos.y())
+
+    def click_event(self, pos:QPoint) -> int:
+        scene_pos = self.mapToScene(pos)
+        
+        for i, rect in enumerate(self.__face_rect):
+            if rect.contains(pos - rect.pos()):
+                return i
