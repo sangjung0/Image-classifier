c02d980c8a5a599004a7c71b0d9d38b03d6d1388
SangJung <sangjung245@gmail.com>
2024-06-08 10:50:24 +0000

    [FEAT] 캐싱 기능 추가

--- program/core/ReverseGeocoder.py
+++ program/core/ReverseGeocoder.py
@@ -1,6 +1,11 @@
 import reverse_geocoder as rg
+import math
 
 class ReverseGeocoder:
+    
+    __GRID_SIZE:int = 1
+    __cache:dict[tuple[int, int], dict[str, str]] = {}
+    
     @staticmethod
     def convert_gps_data(value, ref:str):
         if value is None or ref is None:
@@ -17,16 +22,35 @@ class ReverseGeocoder:
         return decimal_degrees
     
     @staticmethod
+    def get_grid_key(latitude, longitude):
+        
+
+        lat_to_km = 2 * math.pi * 6371.0 / 360.0
+        lon_to_km = 2 * math.pi * 6371.0 * math.cos(math.radians(latitude)) / 360.0
+
+        grid_lat = round(latitude * lat_to_km / ReverseGeocoder.__GRID_SIZE) * ReverseGeocoder.__GRID_SIZE / lat_to_km
+        grid_lon = round(longitude * lon_to_km / ReverseGeocoder.__GRID_SIZE) * ReverseGeocoder.__GRID_SIZE / lon_to_km
+
+        return (grid_lat, grid_lon)
+    
+    @staticmethod
     def get_location_name(latitude, longitude) -> dict:
         """
         위도와 경도를 지역 이름으로 변환
         """
         if latitude is None or longitude is None:
             return None
+        
+        grid_key = ReverseGeocoder.get_grid_key(latitude, longitude)
+        
+        if grid_key in ReverseGeocoder.__cache:
+            return ReverseGeocoder.__cache[grid_key]
 
-        coordinates = (latitude, longitude)
-        result = rg.search(coordinates)
+        result = rg.search([grid_key])
+        
         city = result[0]['name']
         state = result[0]['admin1']
         country = result[0]['cc']
-        return {city, state, country}
+        location = {"city":city, "state":state, "country":country}
+        ReverseGeocoder.__cache[grid_key] = location
+        return location
