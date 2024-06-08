c6be00276427d22027d0916a1aecb96494c78e00
SangJung <sangjung245@gmail.com>
2024-06-08 10:50:12 +0000

    [FEAT] 스토리지 구현

--- program/core/Storage.py
+++ program/core/Storage.py
@@ -1,15 +1,28 @@
 import pathlib
+import pickle
 
 from core.dto import Data
 
 
 class Storage:
-    def __init__(self):
-        self.__path: pathlib.Path
+    def __init__(self, path:pathlib.Path):
+        self.__path: pathlib.Path = path
+        self.__data: Data = None
 
-    def save(self, data: Data): pass
+    def save(self) -> None:
+        if self.__data is not None:
+            with open(self.__path / 'data.pkl', 'wb') as file:
+                pickle.dump(self.__data, file)
 
     def load(self) -> Data:
-        pass
-
-    def get_path(self) -> pathlib.Path: pass
+        path = self.__path / 'data.pkl'
+        if path.exists():
+            try:
+                with open(path , 'rb') as file:
+                    data =  pickle.load(file)
+                    if isinstance(data, Data):
+                        self.__data = data
+                        return data
+            except Exception as _: pass
+        self.__data = Data()
+        return self.__data 
